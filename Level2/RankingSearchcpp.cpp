3a0296bae6a2e6f42f419cdb557dca04d0e80da1
kkw2238@naver.com <kkw2238@naver.com>
2021-06-07 21:53:59 +0900

    21.06.07 Lv2 코드 리팩토링
    ( 31 / 59 ) 완료

--- Level2/RankingSearchcpp.cpp
+++ Level2/RankingSearchcpp.cpp
@@ -1,117 +1,214 @@
-/* https://programmers.co.kr/learn/courses/30/lessons/72412 */
+/* 
+    https://programmers.co.kr/learn/courses/30/lessons/72412 
+*/
 
 #include <string>
 #include <vector>
 #include <map>
+#include <sstream>
 #include <algorithm>
-#include <iostream>
 
 using namespace std;
 
-const string FIELD_ALL = "-";
+const char ALL = '-';
+const int SCROE_INDEX = 4;
 
-class Container {
+class Trie
+{
 public:
-    Container() {};
-    void Initialize(const vector<vector<string>>& fields, int index) {
-        if (index == fields.size())
+    void Initialize(const vector<string>& info, int index)
+    {
+        const char front = info[index].front();
+
+        if (index != SCROE_INDEX)
         {
-            myTier = index;
-            return;
+            nextTier[front].Initialize(info, index + 1);
+            nextTier[ALL].Initialize(info, index + 1);
         }
-
-        for ( string field : fields[index])
+        else
         {
-            careers.insert(pair<string, Container>(field, Container()));
-            careers[field].Initialize(fields, index + 1);
+            scores.push_back(stoi(info[index]));
         }
 
-        myTier = index;
+        initialized = true;
     }
 
-    void Insert(string career) {
-        int index = career.find(" ");
-
-        if (index > -1) {
-            string now = career.substr(0, index);
-            career.replace(career.begin(), career.begin() + index + string(" ").length(), "");
-
-            careers[now].Insert(career);
-            careers[FIELD_ALL].Insert(career);
-        }
-        else {
-            scores.push_back(stoi(career));
-        } 
+    Trie operator[](const char c)
+    {
+        return nextTier[c];
     }
 
-    int Find(vector<string> query) {
-        if (query.size() - 1 > myTier) {
-            return careers[query[myTier]].Find(query);
-        }
-        else {
-            if (query[myTier] == "-") {
-                return scores.size();
-            }
-            else {
-                if (!isSorted) {
-                    sort(scores.begin(), scores.end());
-                    isSorted = true;
-                }
-                return distance(lower_bound(scores.begin(), scores.end(), stoi(query[myTier])), scores.end());
-            }
-        }
+    const int GetOverScoreCount(int n)
+    {
+        return count_if(scores.begin(), scores.end(), [&n](const int score) {
+            return score >= n;
+        });
     }
 
-    map<string, Container> careers;
+public:
     vector<int> scores;
-    int myTier = 0;
-    bool isSorted = false;
+    map<char, Trie> nextTier;
+    bool initialized = false;
 };
 
-vector<string> Split(string s, string c) {
-    Container myContainer;
+int GetCount(Trie& trie, const vector<string>& str)
+{
+    return trie[str[0].front()][str[1].front()][str[2].front()][str[3].front()].GetOverScoreCount(stoi(str[SCROE_INDEX]));
+}
 
+vector<string> Split(string info, const string& demi)
+{
     vector<string> result;
-    int index = s.find(c);
-    
-    while (index > -1) {
-        result.push_back(s.substr(0, index));
-        s.replace(s.begin(), s.begin() + index + c.length(), "");
-        index = s.find(c);
+
+    while (!info.empty())
+    {
+        int index = info.find(demi);
+
+        if (index == string::npos)
+        {
+            break;
+        }
+
+        result.emplace_back(info.substr(0, index));
+
+        string substr = info.substr(index + demi.length());
+        info = substr;
     }
 
-    result.push_back(s);
-
-    if (result.size() > 2) {
-        vector<string> split_blank = Split(result[result.size() - 1], " ");
-        result.erase(result.end() - 1);
-        result.insert(result.end(), split_blank.begin(), split_blank.end());
-    }
+    result.emplace_back(info);
     return result;
 }
 
 vector<int> solution(vector<string> info, vector<string> query) {
-    const vector<vector<string>> FIELDES = {
-        {"cpp", "java", "python", FIELD_ALL},
-        {"backend", "frontend", FIELD_ALL},
-        {"junior", "senior", FIELD_ALL},
-        {"chicken", "pizza", FIELD_ALL}
-    };
+    vector<int> answer;
+    Trie trie;
 
-    vector<int> result;
-    Container myCareers;
-    myCareers.Initialize(FIELDES, 0);
-    
-    for (string inf : info) {
-        myCareers.Insert(inf);
+    for (string& s : info)
+    {
+        trie.Initialize(Split(s, " "), 0);
     }
 
-    for (string que : query) {
-        result.push_back(myCareers.Find(Split(que, " and ")));
+    for (string& q : query)
+    {
+        answer.emplace_back(GetCount(trie, Split(q, "and")));
     }
 
-    return result;
+    return answer;
 }
+
+// 예전 코드
+//#include <string>
+//#include <vector>
+//#include <map>
+//#include <algorithm>
+//#include <iostream>
+//
+//using namespace std;
+//
+//const string FIELD_ALL = "-";
+//
+//class Container {
+//public:
+//    Container() {};
+//    void Initialize(const vector<vector<string>>& fields, int index) {
+//        if (index == fields.size())
+//        {
+//            myTier = index;
+//            return;
+//        }
+//
+//        for ( string field : fields[index])
+//        {
+//            careers.insert(pair<string, Container>(field, Container()));
+//            careers[field].Initialize(fields, index + 1);
+//        }
+//
+//        myTier = index;
+//    }
+//
+//    void Insert(string career) {
+//        int index = career.find(" ");
+//
+//        if (index > -1) {
+//            string now = career.substr(0, index);
+//            career.replace(career.begin(), career.begin() + index + string(" ").length(), "");
+//
+//            careers[now].Insert(career);
+//            careers[FIELD_ALL].Insert(career);
+//        }
+//        else {
+//            scores.push_back(stoi(career));
+//        } 
+//    }
+//
+//    int Find(vector<string> query) {
+//        if (query.size() - 1 > myTier) {
+//            return careers[query[myTier]].Find(query);
+//        }
+//        else {
+//            if (query[myTier] == "-") {
+//                return scores.size();
+//            }
+//            else {
+//                if (!isSorted) {
+//                    sort(scores.begin(), scores.end());
+//                    isSorted = true;
+//                }
+//                return distance(lower_bound(scores.begin(), scores.end(), stoi(query[myTier])), scores.end());
+//            }
+//        }
+//    }
+//
+//    map<string, Container> careers;
+//    vector<int> scores;
+//    int myTier = 0;
+//    bool isSorted = false;
+//};
+//
+//vector<string> Split(string s, string c) {
+//    Container myContainer;
+//
+//    vector<string> result;
+//    int index = s.find(c);
+//    
+//    while (index > -1) {
+//        result.push_back(s.substr(0, index));
+//        s.replace(s.begin(), s.begin() + index + c.length(), "");
+//        index = s.find(c);
+//    }
+//
+//    result.push_back(s);
+//
+//    if (result.size() > 2) {
+//        vector<string> split_blank = Split(result[result.size() - 1], " ");
+//        result.erase(result.end() - 1);
+//        result.insert(result.end(), split_blank.begin(), split_blank.end());
+//    }
+//    return result;
+//}
+//
+//vector<int> solution(vector<string> info, vector<string> query) {
+//    const vector<vector<string>> FIELDES = {
+//        {"cpp", "java", "python", FIELD_ALL},
+//        {"backend", "frontend", FIELD_ALL},
+//        {"junior", "senior", FIELD_ALL},
+//        {"chicken", "pizza", FIELD_ALL}
+//    };
+//
+//    vector<int> result;
+//    Container myCareers;
+//    myCareers.Initialize(FIELDES, 0);
+//    
+//    for (string inf : info) {
+//        myCareers.Insert(inf);
+//    }
+//
+//    for (string que : query) {
+//        result.push_back(myCareers.Find(Split(que, " and ")));
+//    }
+//
+//    return result;
+//}
 
 int main() {
     vector<string> info = {
