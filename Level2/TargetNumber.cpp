cd6b2fb150d218d173049f6bbeeb61c5527b7f9a
kkw2238 <kkw2238@naver.com>
2021-06-10 23:41:46 +0900

    21.06.10 Lv2 코드 리팩토링
    ( 34 / 59 ) 완료

---
+++ Level2/TargetNumber.cpp
@@ -0,0 +1,26 @@
+/*
+	https://programmers.co.kr/learn/courses/30/lessons/43165
+*/
+
+#include <vector>
+
+using namespace std;
+
+int DFS(const vector<int>& numbers, const int sum, const int target, const int index) {
+	int result = 0;
+
+	if (index == numbers.size())
+	{
+		return sum == target ? 1 : 0;
+	}
+
+	result += DFS(numbers, sum + numbers[index], target, index + 1);
+	result += DFS(numbers, sum - numbers[index], target, index + 1);
+
+	return result;
+}
+
+
+int solution(vector<int> numbers, int target) {
+	return DFS(numbers, 0, target, 0);
+}
