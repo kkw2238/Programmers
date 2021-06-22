5982ab1f09e7b7afe6ed3dfe9108d84ab59c3c29
kkw2238@naver.com <kkw2238@naver.com>
2021-06-22 21:56:47 +0900

    21.06.22 Lv2 코드 리팩토링
    ( 39 / 59 ) 완료

--- Level2/More Spicy.cpp
+++ Level2/More Spicy.cpp
@@ -2,31 +2,65 @@
 	https://programmers.co.kr/learn/courses/30/lessons/42626
 */
 
-#include <string>
-#include <algorithm>
-#include <vector>
 #include <set>
+#include <vector>
 
 using namespace std;
 
-int NewScoville(int most, int secondly) {
-	return most + secondly * 2;
+const int NewScoville(int mildestFood, int nextMildestFood)
+{
+    return mildestFood + (nextMildestFood * 2);
 }
 
 int solution(vector<int> scoville, int K) {
-	multiset<int> basket(scoville.begin(), scoville.end());
+    int answer = 0;
+    multiset scovilles(scoville.begin(), scoville.end());
 
-	int answer = 0;
-	while (find_if(basket.begin(), basket.end(), [&K](int a) { return a < K; }) != basket.end()) {
-		if (basket.size() <= 1) return -1;
-		auto most = basket.begin();
-		auto secondly = ++(basket.begin());
+    while (*scovilles.begin() < K)
+    {
+        if (scovilles.size() == 1)
+        {
+            return -1;
+        }
 
-		basket.insert(NewScoville(*most, *secondly));
-		basket.erase(most, ++secondly);
+        int mildestFood = *scovilles.begin();
+        scovilles.erase(scovilles.begin());
 
-		++answer;
-	}
+        int nextMildestFood = *scovilles.begin();
+        scovilles.erase(scovilles.begin());
 
-	return answer;
+        scovilles.insert(NewScoville(mildestFood, nextMildestFood));
+        ++answer;
+    }
+
+    return answer;
 }
+
+//#include <string>
+//#include <algorithm>
+//#include <vector>
+//#include <set>
+//
+//using namespace std;
+//
+//int NewScoville(int most, int secondly) {
+//	return most + secondly * 2;
+//}
+//
+//int solution(vector<int> scoville, int K) {
+//	multiset<int> basket(scoville.begin(), scoville.end());
+//
+//	int answer = 0;
+//	while (find_if(basket.begin(), basket.end(), [&K](int a) { return a < K; }) != basket.end()) {
+//		if (basket.size() <= 1) return -1;
+//		auto most = basket.begin();
+//		auto secondly = ++(basket.begin());
+//
+//		basket.insert(NewScoville(*most, *secondly));
+//		basket.erase(most, ++secondly);
+//
+//		++answer;
+//	}
+//
+//	return answer;
+//}
--- Level2/Transfer Burcket.cpp
+++ Level2/Transfer Burcket.cpp
@@ -7,45 +7,97 @@
 
 using namespace std;
 
-string DistributeUV(string& p)
-{
-	string U = "";
+const int BURCKET_CHAR_START = '(';
 
-	pair<int, int> burcketCount = pair(0, 0);
+bool IsCorrectBurcket(const string& s)
+{
+	int burcketCount[2]{ 0, 0 };
+
+	for (const char c : s)
+	{
+		++burcketCount[c - BURCKET_CHAR_START];
+
+		// ')'이 '(' 갯수보다 많은 경우 => ())( 혹은 )( 와 같은 경우 
+		if (burcketCount[0] < burcketCount[1])
+		{
+			// 올바른 문자열이 아니기에 false반환
+			return false;
+		}
+	}
+
+	return true;
+}
+
+string ReverseBurcket(const string& p)
+{
+	string result = "";
+
+	const char burckets[2]{ ')', '(' };
 
 	for (const char c : p)
 	{
-		if (c == '(')
-		{
-			++burcketCount.first;
-		}
-		else
-		{
-			++burcketCount.second;
-		}
+		// ')' '(' 에 따라 반대 괄호를 삽입
+		result.push_back(burckets[(int)c - BURCKET_CHAR_START]);
+	}
 
-		U.push_back(c);
+	return result;
+}
 
-		if (burcketCount.first == burcketCount.second)
+string DistributeUV(string& p)
+{
+	// Step1. p 가 비어있는 경우 p를 반환
+	if (p.empty())
+	{
+		return p;
+	}
+
+	string U = "";
+	string V = "";
+	int burcketCount[2]{ 0, 0 };
+
+	// Step2. 균형잡힌 괄호 문자열 U와 나머지 V로 분리
+	for (size_t i = 0; i < p.size(); ++i)
+	{
+		// 균형잡힌 괄호 문자열 판단을 위한 카운팅
+		++burcketCount[p[i] - BURCKET_CHAR_START];
+
+		U.push_back(p[i]);
+
+		if (burcketCount[0] == burcketCount[1])
 		{
-			return U;
+			// U를 제외한 나머지를 V에 저장
+			V = p.substr(i + 1);
+			break;
 		}
+	}
+
+	// V에 대해 1부터 다시 시작( U가 올바른 괄호 문자열 여부 상관 없이 V는 step1부터 다시 해야한다. )
+	V = DistributeUV(V);
+
+	// Step3. U가 올바른 괄호 문자열인 경우 
+	if (IsCorrectBurcket(U))
+	{
+		// Step3 - 1. 1단계부터 시작한 V를 뒤에 연결
+		U.append(V);
+	}
+	// Step4. U가 올바르지 못한 괄호 문자열인 경우
+	else
+	{
+		// Step4 - 1. 빈문자열에 '(' 추가
+		string newp = "(";
+
+		// Step4 - 2, 3, Step 1부터 시작한 V와 ')' 를 뒤에 붙여준다.
+		newp.append(V + ")");
+		// Step4 - 4. U의 맨 앞,뒤 문자열을 자르고 남은 문자열에 대해 괄호를 뒤집어 뒤에 붙인다.
+		newp.append(ReverseBurcket(U.substr(1, U.length() - 2)));
+		U = newp;
 	}
 
 	return U;
 }
 
 string solution(string p) {
-	string answer = "";
-	
-	if (p.empty())
-	{
-		return p;
-	}
-
-
-
-	return answer;
+	return DistributeUV(p);
 }
 
 /*
