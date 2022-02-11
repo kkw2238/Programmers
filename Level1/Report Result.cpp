5564ac93510adfc9138f8bad5062809832fe5204
kkw2238@naver.com <kkw2238@naver.com>
2022-02-11 20:08:09 +0900

    2022.02.11 Lv2 코드 풀이
    Lv1. 신고 결과 받기
    Lv2. 빛의 사이클 문제 해결

---
+++ Level1/Report Result.cpp
@@ -0,0 +1,48 @@
+/*
+    https://programmers.co.kr/learn/courses/30/lessons/92334
+*/
+
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+
+using namespace std;
+
+vector<int> solution(vector<string> id_list, vector<string> report, int k) {
+    map<string, set<string>> reportResult;
+    map<string, int> successReport;
+    int id_count = id_list.size();
+
+    for (const string& rep : report)
+    {
+        int voidIndex = rep.find(' ');
+        
+        string from = rep.substr(0, voidIndex);
+        string to = rep.substr(voidIndex + 1);
+
+        reportResult[to].insert(from);
+    }
+
+    for (auto result : reportResult)
+    {
+        if (result.second.size() < k)
+        {
+            continue;
+        }
+
+        for (const string& id : result.second)
+        {
+            ++successReport[id];
+        }
+    }
+
+    vector<int> answer(id_count);
+    for (int i = 0; i < id_count; ++i)
+    {
+        answer[i] = successReport[id_list[i]];
+    }
+
+
+    return answer;
+}
--- Level2/Light Cycle.cpp
+++ Level2/Light Cycle.cpp
@@ -5,34 +5,43 @@
 #include <string>
 #include <array>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-/*
-각 칸마다 S, L, 또는 R가 써져 있는 격자가 있습니다. 당신은 이 격자에서 빛을 쏘고자 합니다. 이 격자의 각 칸에는 다음과 같은 특이한 성질이 있습니다.
-
-빛이 "S"가 써진 칸에 도달한 경우, 직진합니다.
-빛이 "L"이 써진 칸에 도달한 경우, 좌회전을 합니다.
-빛이 "R"이 써진 칸에 도달한 경우, 우회전을 합니다.
-빛이 격자의 끝을 넘어갈 경우, 반대쪽 끝으로 다시 돌아옵니다. 예를 들어, 빛이 1행에서 행이 줄어드는 방향으로 이동할 경우, 같은 열의 반대쪽 끝 행으로 다시 돌아옵니다.
-당신은 이 격자 내에서 빛이 이동할 수 있는 경로 사이클이 몇 개 있고, 각 사이클의 길이가 얼마인지 알고 싶습니다. 경로 사이클이란, 빛이 이동하는 순환 경로를 의미합니다.
-
-예를 들어, 다음 그림은 격자 ["SL","LR"]에서 1행 1열에서 2행 1열 방향으로 빛을 쏠 경우, 해당 빛이 이동하는 경로 사이클을 표현한 것입니다.
-*/
-
 enum { UP, RIGHT, DOWN, LEFT };
-using Board = vector<vector<vector<int>>>;
+using Board = vector<vector<vector<bool>>>;
 
 array<int, 4> LightDirection{ UP, RIGHT, DOWN, LEFT };
 Board board;
 const int DIRECTION = 4;
+int board_Width, board_Height;
 
+// Reflection : 해당 위치에 도달한 빛이 진행할 방향을 반환해주는 함수
 const int Refelction(const int inputDirection, const char reflectDirection)
 {
     switch (reflectDirection)
-    {
+    {       
     case 'S':
+        // S : 들어온 방향 그대로 진행한다.
         return inputDirection;
+
+        /*
+            L, R 이전 블럭에서 쏜 방향에 따라 다른 진행 방향을 리턴해 주는데 
+            Input과 Output의 리턴 관계는 아래와 같다.
+
+            UP - L :      ← L < reflectDirection
+                            ↑ < inputDirection
+                => Return L
+
+            DOWN - L :      ↓ < inputDirection
+                            L → < reflectDirection
+                => Return R
+
+                ...
+
+                LightDirection 배열 기준으로 inputDirection의 -1 위치에 반사광이 진행할 방향값이 들어있다.
+        */
     case 'L':
         return (inputDirection + DIRECTION - 1) % DIRECTION;
     case 'R':
@@ -40,29 +49,80 @@ const int Refelction(const int inputDirection, const char reflectDirection)
     }
 }
 
-const int Move(const int xPos, const int yPos, const int direction)
+// Move : 빛의 방향에 따라 진행한 결과를 반환해주는 함수
+const void Move(int& xPos, int& yPos, const int direction)
 {
+    /*
+        y = 0인 위치에서 위로 쏘거나 y = height인 위치에서 빛을 아래로 쏠 경우 
+        반대쪽으로 순환하게 만들어야 하기에
+        % board_Height or % board_Width 연산을 통해 순환
+    */
     switch (direction)
     {
-    default:
+    case UP :
+        yPos = (yPos + board_Height - 1) % board_Height;
+        break;
+    case DOWN:
+        yPos = (yPos + 1) % board_Height;
+        break;
+    case LEFT :
+        xPos = (xPos + board_Width - 1) % board_Width;
+        break;
+    case RIGHT :
+        xPos = (xPos + 1) % board_Width;
         break;
     }
 }
 
+// Light : 시작 위치에서 direction 방향으로 빛을 쏠 경우 몇 번 이동해야 빛이 순환되는지 알려주는 함수
 const int Lighting(vector<string> grid, int xPos, int yPos, int direction)
 {
-    if (board[yPos][xPos][direction] > 0)
-        return 0;
+    int moveDist = 0;
 
-    ++board[yPos][xPos][direction];
+    // 이미 지나간 곳인지 확인
+    while (!board[yPos][xPos][direction])
+    {
+        // 지나간 곳을 체크 해준다.
+        board[yPos][xPos][direction] = true;
 
-    
+        // 이동
+        Move(xPos, yPos, direction);
+        // 반사
+        direction = Refelction(direction, grid[yPos][xPos]);
+
+        ++moveDist;
+    }
+
+    return moveDist;
 }
 
 vector<int> solution(vector<string> grid) {
-    const int LENGTH = grid[0].length();
-    board = Board(DIRECTION, vector<vector<int>>(LENGTH, vector<int>(LENGTH, 0)));
+    board_Width = grid[0].length();
+    board_Height = grid.size();
+
+    board = Board(board_Height, vector<vector<bool>>(board_Width, vector<bool>(DIRECTION, 0)));
     vector<int> answer;
+
+    for (int y = 0; y < board_Height; ++y)
+    {
+        for (int x = 0; x < board_Width; ++x)
+        {
+            for (int dir = 0; dir < DIRECTION; ++dir)
+            {
+                if (board[y][x][dir])
+                    continue;
+
+                int dist = Lighting(grid, x, y, dir);
+                
+                if (dist > 0)
+                {
+                    answer.push_back(dist);
+                }
+            }
+        }
+    }
+
+    sort(answer.begin(), answer.end());
 
     return answer;
 }
