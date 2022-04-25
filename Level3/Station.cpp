83f48d6edb6a5433c5ffc5c2b3962534a5d83f48
kkw2238@naver.com <kkw2238@naver.com>
2022-04-25 20:31:19 +0900

    2022.04.25 Lv3. 코드 풀이
    Lv3. 기지국 설치 해결

--- Level3/Station.cpp
+++ Level3/Station.cpp
@@ -1,12 +1,73 @@
-#include <iostream>
+/*
+	https://programmers.co.kr/learn/courses/30/lessons/12979
+*/
+
 #include <vector>
+#include <queue>
+
 using namespace std;
 
+struct WifiRange {
+	int beginWifiIndex;
+	int endWifiIndex;
+};
+
+int GetEstablishStationCount(int offset, int range)
+{
+	int stationCount = offset / range;
+
+	if (offset % range > 0)
+	{
+		++stationCount;
+	}
+
+	return stationCount;
+}
+
+int solution(int n, vector<int> stations, int w)
+{
+	queue<WifiRange> wifiStation;
+
+	int answer = 0;
+	int memEndIndex = 1;
+
+	for (int station : stations)
+	{
+		int beginWifi = max(station - w, 1);
+		int endWifi = min(station + w, n);
+
+		wifiStation.push({ beginWifi, endWifi });
+	}
+	
+	while (!wifiStation.empty())
+	{
+		WifiRange stationRange = wifiStation.front();
+		wifiStation.pop();
+
+		if (stationRange.beginWifiIndex > memEndIndex)
+		{
+			int offset = stationRange.beginWifiIndex - memEndIndex;
+			answer += GetEstablishStationCount(offset, w * 2 + 1);
+		}
+
+		memEndIndex = stationRange.endWifiIndex;
+	}
+
+	if (memEndIndex != n)
+	{
+		int offset = n - memEndIndex;
+		answer += GetEstablishStationCount(offset, w * 2 + 1);
+	}
+
+	return answer;
+}
+
+/*
 const int STATION = 1;
 const int WAVE_REACH = 2;
 
 void BuildStation(const int n, vector<int>& waveReach, const int pos, const int w) {
-	for (int i = 0; i <= w; ++i) 
+	for (int i = 0; i <= w; ++i)
 	{
 		if (i == 0)
 		{
@@ -40,7 +101,7 @@ int solution(int n, vector<int> stations, int w)
 	int result = 0;
 	vector<int> waveReach(n);
 
-	for (int station : stations) 
+	for (int station : stations)
 	{
 		BuildStation(n, waveReach, station - 1, w);
 	}
@@ -63,12 +124,4 @@ int solution(int n, vector<int> stations, int w)
 	return result;
 }
 
-#include <iostream>
-int main()
-{
-	int n = 11;
-	vector<int> stations{ 4, 11 };
-	int w = 1;
-
-	cout << solution(n, stations, w);
-}
+*/
