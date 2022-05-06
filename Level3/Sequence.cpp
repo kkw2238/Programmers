42161fb36fbf541e497ee001233a858e700f0a88
kkw2238@naver.com <kkw2238@naver.com>
2022-05-06 23:49:47 +0900

    2022.05.06 Lv3. 코드 풀이
    Lv3. 스타수열 풀이 중

--- Level3/Sequence.cpp
+++ Level3/Sequence.cpp
@@ -107,82 +107,212 @@
 //	return answer;
 //}
 
+//#include <vector>
+//#include <set>
+//
+//using namespace std;
+//
+//const bool hasCross(const pair<int, int>& p, const int pairCount, const vector<int>& cross)
+//{
+//	return cross[p.first] == pairCount || cross[p.second] == pairCount || cross.empty();
+//}
+//
+//int maxPairCount = 0;
+//
+//int makeSequence(const vector<int>& vi, int index, int pairCount, vector<set<int>>& sequence, int beforeNum, vector<int>& cross)
+//{
+//	pair<int, int> pi;
+//	int result = pairCount;
+//
+//	if (beforeNum != -1)
+//	{
+//		if (beforeNum == vi[index])
+//		{
+//			return result;
+//		}
+//
+//		pi.first = beforeNum;
+//		pi.second = vi[index];
+//
+//		if (hasCross(pi, pairCount, cross))
+//		{
+//			if (sequence[pi.first].find(pi.second) == sequence[pi.first].end())
+//			{
+//				++cross[pi.first];
+//				++cross[pi.second];
+//
+//				++pairCount;
+//				maxPairCount = max(pairCount, pairCount);
+//
+//				for (int i = index + 1; i < vi.size(); ++i)
+//				{
+//					result = max(result, makeSequence(vi, i, pairCount, sequence, -1, cross));
+//				}
+//
+//				--cross[pi.first];
+//				--cross[pi.second];
+//				sequence[pi.first].erase(pi.second);
+//			}
+//		}
+//	}
+//	else
+//	{
+//		for (int i = index + 1; i < vi.size(); ++i)
+//		{
+//			result = max(result, makeSequence(vi, i, pairCount, sequence, vi[index], cross));
+//		}
+//	}
+//
+//	return max(result, pairCount);
+//}
+//
+//int solution(vector<int> a) {
+//	int answer = -1;
+//	vector<set<int>> sequence(a.size(), set<int>());
+//	vector<int> cross(a.size());
+//
+//	for (int i = 0; i < a.size(); ++i) 
+//	{
+//		answer = max(answer, makeSequence(a, 0, 0, sequence, -1, cross));
+//	}
+//
+//	return answer * 2;
+//}
+
 #include <vector>
 #include <set>
+#include <map>
+#include <algorithm>
 
 using namespace std;
+//
+//int totalCount = 0;
+//
+//int makeSequence(const vector<int>& vi, int idx, const vector<int>& indices, set<pair<int, int>>& makedSequences, int beforeIndex)
+//{
+//	set<pair<int, int>> makeSequences;
+//	int beginIndex = -1;
+//	const int sequenceSize = makedSequences.size();
+//	int result = sequenceSize;
+//
+//	if (vi.size() == 1)
+//	{
+//		return 0;
+//	}
+//	if (indices.size() - idx < totalCount)
+//	{
+//		return result;
+//	}
+//
+//	for (int i = idx; i < indices.size(); ++i)
+//	{
+//		int nowIndex = indices[i];
+//
+//		if (nowIndex > 0 && vi[nowIndex - 1] != vi[nowIndex] && (nowIndex - 1) != beforeIndex)
+//		{
+//			makedSequences.insert(pair(vi[nowIndex - 1], vi[nowIndex]));
+//			if (sequenceSize != makedSequences.size())
+//			{
+//				result = max(result, makeSequence(vi, i + 1, indices, makedSequences, nowIndex));
+//				makedSequences.erase(pair(vi[nowIndex - 1], vi[nowIndex]));
+//			}
+//		}
+//		else if (nowIndex < (vi.size() - 1) && vi[nowIndex] != vi[nowIndex + 1])
+//		{
+//			makedSequences.insert(pair(vi[nowIndex], vi[nowIndex + 1]));
+//			if (sequenceSize != makedSequences.size())
+//			{
+//				result = max(result, makeSequence(vi, i + 1, indices, makedSequences, nowIndex + 1));
+//				makedSequences.erase(pair(vi[nowIndex], vi[nowIndex + 1]));
+//			}
+//		}
+//	}
+//
+//	if (result > totalCount)
+//	{
+//		totalCount = result;
+//	}
+//	return result;
+//}
+//
+int totalCount = 0;
 
-const bool hasCross(const pair<int, int>& p, const int pairCount, const vector<int>& cross)
+int makeSequence(const vector<int>& vi, int idx, const vector<int>& indices, int beforeIndex)
 {
-	return cross[p.first] == pairCount || cross[p.second] == pairCount || cross.empty();
-}
+	set<pair<int, int>> makeSequences;
+	int beginIndex = -1;
+	
+	int result = 0;
 
-int maxPairCount = 0;
-
-int makeSequence(const vector<int>& vi, int index, int pairCount, vector<set<int>>& sequence, int beforeNum, vector<int>& cross)
-{
-	pair<int, int> pi;
-	int result = pairCount;
-
-	if (beforeNum != -1)
+	if (vi.size() == 1)
 	{
-		if (beforeNum == vi[index])
+		return 0;
+	}
+	
+	for (int i = idx; i < indices.size(); ++i)
+	{
+		int sequenceSize = makeSequences.size();
+		result = sequenceSize;
+
+		if (result > totalCount)
+		{
+			totalCount = result;
+		}
+		if ((indices.size() - i + result) < totalCount)
 		{
 			return result;
 		}
 
-		pi.first = beforeNum;
-		pi.second = vi[index];
+		int nowIndex = indices[i];
 
-		if (hasCross(pi, pairCount, cross))
+		if (nowIndex > 0 && vi[nowIndex - 1] != vi[nowIndex] && (nowIndex - 1) != beforeIndex)
 		{
-			if (sequence[pi.first].find(pi.second) == sequence[pi.first].end())
-			{
-				++cross[pi.first];
-				++cross[pi.second];
-
-				++pairCount;
-				maxPairCount = max(pairCount, pairCount);
-
-				for (int i = index + 1; i < vi.size(); ++i)
-				{
-					result = max(result, makeSequence(vi, i, pairCount, sequence, -1, cross));
-				}
-
-				--cross[pi.first];
-				--cross[pi.second];
-				sequence[pi.first].erase(pi.second);
-			}
+			makeSequences.insert(pair(vi[nowIndex - 1], vi[nowIndex]));
+			beforeIndex = nowIndex;
 		}
-	}
-	else
-	{
-		for (int i = index + 1; i < vi.size(); ++i)
+		else if (nowIndex < (vi.size() - 1) && vi[nowIndex] != vi[nowIndex + 1])
 		{
-			result = max(result, makeSequence(vi, i, pairCount, sequence, vi[index], cross));
+			makeSequences.insert(pair(vi[nowIndex], vi[nowIndex + 1]));
+			beforeIndex = nowIndex + 1;
 		}
 	}
 
-	return max(result, pairCount);
+	return result;
 }
 
 int solution(vector<int> a) {
-	int answer = -1;
-	vector<set<int>> sequence(a.size(), set<int>());
-	vector<int> cross(a.size());
+	int result = 0;
 
-	for (int i = 0; i < a.size(); ++i) 
+	map<int, vector<int>> eleIndex;
+
+	for (int i = 0; i < a.size(); ++i)
 	{
-		answer = max(answer, makeSequence(a, 0, 0, sequence, -1, cross));
+		eleIndex[a[i]].push_back(i);
 	}
 
-	return answer * 2;
+	vector<pair<int, vector<int>>> vpiv( eleIndex.begin(), eleIndex.end() );
+	sort(vpiv.begin(), vpiv.end(), [](const pair<int, vector<int>>& l, const pair<int, vector<int>>& r) {
+		return l.second.size() > r.second.size();
+	});
+
+	for (const auto& indices : vpiv) {
+		if ((indices.second.size() * 2) < result)
+		{
+			break;
+		}
+		totalCount = 0;
+		result = max(result, makeSequence(a, 0, indices.second, -1) * 2);
+	}
+
+	return result;
 }
+
+#include <iostream>
 
 int main()
 {
-	vector<int> a{ 1, 1, 1, 2, 1, 3 };
-	solution(a);
+	vector<int> a{ 0, 0, 3, 1, 2, 1, 3, 4, 0, 1, 4 };
+	cout << solution(a);
 }
 /*
 	vector<vector<int>> 각 항목별 index를 기억
