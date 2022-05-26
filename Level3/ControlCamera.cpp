68571e68b1400fc286033217a52f8d56c468461c
kkw2238@naver.com <kkw2238@naver.com>
2022-05-26 21:15:36 +0900

    2022.05.26 Lv3 코드 풀이
    Lv3. 단속카메라 문제 해결
    Lv3. 보석 쇼핑 문제 풀이 중

--- Level3/ControlCamera.cpp
+++ Level3/ControlCamera.cpp
@@ -48,34 +48,87 @@
 //    return answer;
 //}
 
+
+//int getMaximumIndex(vector<int>& prefix, int minRange, int maxRange)
+//{
+//    int memprefix = 0;
+//    int maximum = 0;
+//    int maxIndex = -1;
+//
+//    for (int i = minRange; i <= maxRange + 1; ++i)
+//    {
+//        memprefix += prefix[i];
+//
+//        if (maximum < memprefix)
+//        {
+//            maximum = memprefix;
+//            maxIndex = i;
+//        }
+//    }
+//
+//    return maxIndex;
+//}
+//
+//struct Cmp
+//{
+//    const bool operator()(const vector<int>& a, const vector<int>& b) const
+//    {
+//        return a[0] < b[0];
+//    }
+//};
+//
+//#include <iostream>
+//
+//
+//vector<vector<int>> init(multiset<vector<int>, Cmp>& road, int minRange, int maxRange)
+//{
+//    int range = maxRange - minRange;
+//    int tier = 0;
+//    int tmpRange = range;
+//
+//    vector<vector<int>> tree;
+//
+//    while (range > 1)
+//    {
+//        tree.push_back(vector<int>(range + (range % 2) + 1));
+//        range = range / 2 + (range % 2);
+//        ++tier;
+//    }
+//    tree.push_back(vector<int>(range + 1));
+//
+//    for (const vector<int>& log : road)
+//    {
+//        int leftIndex = log[0] - minRange;
+//        int rightIndex = log[1] - minRange + 1;
+//
+//        for (int i = 0; i < tier; ++i)
+//        {
+//            ++tree[i][leftIndex];
+//            --tree[i][rightIndex];
+//
+//            leftIndex = leftIndex / 2;
+//            rightIndex = rightIndex / 2;
+//        }
+//    }
+//
+//    for (vector<int>& vi : tree)
+//    {
+//        for (int i : vi)
+//        {
+//            cout << i << ' ';
+//        }
+//        cout << '\n';
+//    }
+//
+//    return tree;
+//}
+
 #include <vector>
 #include <algorithm>
+#include <queue>
 #include <set>
 
 using namespace std;
-
-const int OFFSET = 30001;
-const int ROAD_DISTANCE = OFFSET * 2;
-
-int getMaximumIndex(vector<int>& prefix, int minRange, int maxRange)
-{
-    int memprefix = 0;
-    int maximum = 0;
-    int maxIndex = -1;
-
-    for (int i = minRange; i <= maxRange + 1; ++i)
-    {
-        memprefix += prefix[i];
-
-        if (maximum < memprefix)
-        {
-            maximum = memprefix;
-            maxIndex = i;
-        }
-    }
-
-    return maxIndex;
-}
 
 struct Cmp
 {
@@ -85,120 +138,74 @@ struct Cmp
     }
 };
 
-#include <iostream>
-
-
-vector<vector<int>> init(multiset<vector<int>, Cmp>& road, int minRange, int maxRange)
+int getMaximumIndex(multiset<vector<int>, Cmp>& sorted_routes)
 {
-    int range = maxRange - minRange;
-    int tier = 0;
-    int tmpRange = range;
+    priority_queue<int> exitIndices;
+    int count = 0;
+    int maximum_index = 0;
+    int maximum_count = 0;
 
-    vector<vector<int>> tree;
-
-    while (range > 1)
+    for (const vector<int>& route : sorted_routes)
     {
-        tree.push_back(vector<int>(range + (range % 2) + 1));
-        range = range / 2 + (range % 2);
-        ++tier;
-    }
-    tree.push_back(vector<int>(range + 1));
+        int startIndex = route[0];
+        int exitIndex = route[1];
 
-    for (const vector<int>& log : road)
-    {
-        int leftIndex = log[0] - minRange;
-        int rightIndex = log[1] - minRange + 1;
-
-        for (int i = 0; i < tier; ++i)
+        while (!exitIndices.empty() && exitIndices.top() < startIndex)
         {
-            ++tree[i][leftIndex];
-            --tree[i][rightIndex];
+            exitIndices.pop();
+            --count;
+        }
 
-            leftIndex = leftIndex / 2;
-            rightIndex = rightIndex / 2;
+        exitIndices.push(exitIndex);
+        ++count;
+
+        if (count > maximum_count)
+        {
+            maximum_count = count;
+            maximum_index = startIndex;
         }
     }
 
-    for (vector<int>& vi : tree)
-    {
-        for (int i : vi)
-        {
-            cout << i << ' ';
-        }
-        cout << '\n';
-    }
-
-    return tree;
+    return maximum_index;
 }
 
-int findMaximum(vector<vector<int>>& tree)
+const bool isInside(const vector<int>& range, int index)
 {
-    int maximum = -INT_MAX;
-    int count = 0;
-    int xIndex = 0, yIndex = 0;
+    return range[0] <= index && range[1] >= index;
+}
 
-    for (int y = tree.size() - 1; y >= 0; --y)
+void eraseDetectedCar(multiset<vector<int>, Cmp>& sorted_routes, int index)
+{
+    for (auto iter = sorted_routes.begin(); iter != sorted_routes.end();)
     {
-        for (int x = 0; x < tree[y].size(); ++x)
+        vector<int> route = *iter;
+
+        if (route[0] > index)
         {
-            if (tree[y][x] > maximum)
-            {
-                maximum = tree[y][x];
-                xIndex = x;
-                yIndex = y;
-            }
+            break;
+        }
+        else if (isInside(route, index))
+        {
+            auto tmpIter = iter++;
+            sorted_routes.erase(tmpIter);
+        }
+        else
+        {
+            ++iter;
         }
     }
-
-
-
 }
 
 int solution(vector<vector<int>> routes) {
-    multiset<vector<int>, Cmp> road(routes.begin(), routes.end());
-    int minRange = OFFSET;
-    int maxRange = -OFFSET;
+    multiset<vector<int>, Cmp> sorted_routes(routes.begin(), routes.end());
+    int count = 0;
 
-    for (vector<int>& route : routes)
+    while (!sorted_routes.empty())
     {
-        minRange = min(minRange, route[0]);
-        maxRange = max(maxRange, route[1] + 1);
+        int index = getMaximumIndex(sorted_routes);
+        eraseDetectedCar(sorted_routes, index);
+        ++count;
     }
 
-    vector<vector<int>> tree = init(road, minRange, maxRange);
-
-    return 0;
+    return count;
 }
-
-#include <iostream>
-
-int main()
-{
-    cout << solution({ {-20, 15}, {-14, -5}, {-18, -13}, {-5, -3} }); //2
-    cout << endl;
-    cout << solution({ {-2, -1}, {1, 2}, {-3, 0} });//2
-    cout << endl;
-    cout << solution({ {0, 0}, });//1
-    cout << endl;
-    cout << solution({ {0, 1}, {0, 1}, {1, 2} });//1
-    cout << endl;
-    cout << solution({ {0, 1}, {2, 3}, {4, 5}, {6, 7} });//4
-    cout << endl;
-    cout << solution({ {-20, -15}, {-14, -5}, {-18, -13}, {-5, -3} });//2
-    cout << endl;
-    cout << solution({ {-20, 15}, {-20, -15}, {-14, -5}, {-18, -13}, {-5, -3} });//2
-    cout << endl;
-    cout << solution({ {2,2},{0,1},{-10,9} });//2
-    cout << endl;
-    cout << solution({ {-7, 0}, {-6, -4}, {-5, -3}, {-3, -1}, {-1, 4}, {1, 2}, {3, 4} });//4
-    cout << endl;
-    cout << solution({ {-5, -3}, {-4, 0}, {-4, -2}, {-1, 2}, {0, 3}, {1, 5}, {2, 4} });//2
-    cout << endl;
-    cout << solution({ {0, 1}, {1, 2}, {2, 3}, {3, 4}, {4, 5}, {5, 6}, {6, 7}, {7, 8}, {8, 9}, {9, 10}, {10, 11}, {11, 12}, {12, 13}, {13, 14}, {14, 15} });//8
-    cout << endl;
-    cout << solution({ {0, 12}, {1, 12}, {2, 12}, {3, 12}, {5, 6}, {6, 12}, {10, 12} });//2
-    cout << endl;
-    cout << solution({ {-191, -107}, { -184,-151 }, { -150,-102 }, { -171,-124 }, { -120,-114 } }); // 2
-    cout << endl;
-}
-
--- Level3/JewerlyShopping.cpp
+++ Level3/JewerlyShopping.cpp
@@ -1,30 +1,82 @@
+//#include <string>
+//#include <vector>
+//#include <set>
+//
+//using namespace std;
+//
+//vector<int> solution(vector<string> gems) {
+//    set<string> kindofGem(gems.begin(), gems.end());
+//    int length = kindofGem.size();
+//    while (true) 
+//    {
+//        for (int i = 0; i <= gems.size() - length; ++i)
+//        {
+//            set<string> shoppingGems(gems.begin() + i, gems.begin() + i + length);
+//            if (shoppingGems.size() == kindofGem.size())
+//            {
+//                return vector<int>{i + 1, i + length};
+//            }
+//        }
+//        ++length;
+//    }
+//
+//    return vector<int>{ 1, static_cast<int>(gems.size()) };
+//}
+
 #include <string>
 #include <vector>
 #include <set>
+#include <algorithm>
 
 using namespace std;
 
-vector<int> solution(vector<string> gems) {
-    set<string> kindofGem(gems.begin(), gems.end());
-    int length = kindofGem.size();
-    while (true) 
+int leftIndex = 0;
+int rightIndex = 0;
+int maximum = 100001;
+
+int findSide(vector<string>& gems, int left, int right, set<string>& typeOfgems)
+{
+    set<string> typeOfInsideGems = set<string>(gems.begin() + left, gems.begin() + right);
+
+    int range = right - left;
+    int mid = (left + right) / 2;
+
+    if (typeOfInsideGems.size() != typeOfgems.size() || range < typeOfgems.size())
     {
-        for (int i = 0; i <= gems.size() - length; ++i)
-        {
-            set<string> shoppingGems(gems.begin() + i, gems.begin() + i + length);
-            if (shoppingGems.size() == kindofGem.size())
-            {
-                return vector<int>{i + 1, i + length};
-            }
-        }
-        ++length;
+        return 100001;
     }
 
-    return vector<int>{ 1, static_cast<int>(gems.size()) };
+    vector<int> leftSide{ mid + 1, left, left + ((mid - left) / 2), left };
+    vector<int> rightSide{ right, mid - 1, right, mid + ((right - mid) / 2) };
+
+    for (int i = 3; i >= 0; --i)
+    {
+        range = min(findSide(gems, leftSide[i], rightSide[i], typeOfgems), range);
+
+        if (range < maximum)
+        {
+            maximum = range;
+            leftIndex = leftSide[i];
+            rightIndex = rightSide[i];
+        }
+    }
+    
+    return range;
 }
+
+
+vector<int> solution(vector<string> gems) {
+    set<string> typeOfGems(gems.begin(), gems.end());
+
+    findSide(gems, 0, gems.size(), typeOfGems);
+
+    return vector<int>{leftIndex + 1, rightIndex + 1};
+}
+
+
 
 int main()
 {
-    vector<string> gems{ "ZZZ", "YYY", "NNNN", "YYY", "BBB" };
+    vector<string> gems{ "XYZ", "XYZ", "XYZ" };
     solution(gems);
 }
