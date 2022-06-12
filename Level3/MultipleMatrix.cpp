9d4b7fab548a47e3789f756fe4f6843fcd350412
kkw2238@naver.com <kkw2238@naver.com>
2022-06-12 20:39:56 +0900

    2022.06.12 Lv3 코드 풀이
    Lv3. 최적의 행렬 곱셈 문제 풀이중

--- Level3/MultipleMatrix.cpp
+++ Level3/MultipleMatrix.cpp
@@ -7,50 +7,81 @@
 
 using namespace std;
 
-const int MAXINT = 2147483647;
+const int MAXIMUM = 201;
 
-const int getMulCount(const vector<int>& a, const vector<int>& b)
+int memory[MAXIMUM][MAXIMUM];
+
+int minimum(int start, int mid, int end, int dk)
 {
-    return a[0] * a[1] * b[1];
+    return 0;
 }
 
-int solution(vector<vector<int>> matrix_sizes) {
-    int answer = MAXINT;
-    int count = 0;
-    vector<int> permutationData;
+int mul(vector<int>& d)
+{
 
-    for (int i = 0; i < matrix_sizes.size() - 1; ++i)
+
+    return 0;
+}
+
+int solution(vector<vector<int>> matrix_sizes) 
+{
+    vector<int> d(matrix_sizes.size() + 1);
+    d[0] = matrix_sizes[0][0];
+    
+    for (int i = 0; i < matrix_sizes.size(); ++i)
     {
-        permutationData.push_back(i);
+        d[i + 1] = matrix_sizes[i][1];
     }
-
-    do {
-        cout << count++ << '\n';
-        vector<vector<int>> tmpMatrix_sizes = matrix_sizes;
-        int tmpResult = 0;
-        for (int i = 0; i < permutationData.size(); ++i)
-        {
-            int index = permutationData[i];
-
-            tmpResult += getMulCount(tmpMatrix_sizes[index], tmpMatrix_sizes[index + 1]);
-
-            if (tmpResult > answer)
-            {
-                break;
-            }
-
-            tmpMatrix_sizes[index] = { tmpMatrix_sizes[index][0], tmpMatrix_sizes[index + 1][1] };
-            tmpMatrix_sizes[index + 1] = { tmpMatrix_sizes[index][0], tmpMatrix_sizes[index + 1][1] };
-        }
-
-        answer = min(tmpResult, answer);
-    } while (next_permutation(permutationData.begin(), permutationData.end()));
-
-    return answer;
+    
+    return 0;
 }
+
+//
+//
+//const int MAXINT = 2147483647;
+//
+//const int getMulCount(const vector<int>& a, const vector<int>& b)
+//{
+//    return a[0] * a[1] * b[1];
+//}
+//
+//int solution(vector<vector<int>> matrix_sizes) {
+//    int answer = MAXINT;
+//    int count = 0;
+//    vector<int> permutationData;
+//
+//    for (int i = 0; i < matrix_sizes.size() - 1; ++i)
+//    {
+//        permutationData.push_back(i);
+//    }
+//
+//    do {
+//        vector<vector<int>> tmpMatrix_sizes = matrix_sizes;
+//        int tmpResult = 0;
+//        for (int i = 0; i < permutationData.size(); ++i)
+//        {
+//            int index = permutationData[i];
+//
+//            tmpResult += getMulCount(tmpMatrix_sizes[index], tmpMatrix_sizes[index + 1]);
+//
+//            if (tmpResult > answer)
+//            {
+//                break;
+//            }
+//
+//            tmpMatrix_sizes[index] = { tmpMatrix_sizes[index][0], tmpMatrix_sizes[index + 1][1] };
+//            tmpMatrix_sizes[index + 1] = { tmpMatrix_sizes[index][0], tmpMatrix_sizes[index + 1][1] };
+//        }
+//
+//        answer = min(tmpResult, answer);
+//    } while (next_permutation(permutationData.begin(), permutationData.end()));
+//
+//    return answer;
+//}
 
 int main()
 {
+    
     solution({ {5, 3}, {3,10}, {10,6}, {5, 3}, {3,10}, {10,6}, {5, 3}, {3,10}, {10,6}, {5, 3}, {3,10}, {10,6}, {5, 3}, {3,10}, {10,6}, {5, 3}, {3,10}, {10,6}, {5, 3}, {3,10}, {10,6}, {5, 3}, {3,10}, {10,6}, {5, 3}, {3,10}, {10,6}, {5, 3}, {3,10}, {10,6}, {5, 3}, {3,10}, {10,6}, {5, 3}, {3,10}, {10,6}, {5, 3}, {3,10}, {10,6}, {5, 3}, {3,10}, {10,6}, {5, 3}, {3,10}, {10,6}, {5, 3}, {3,10}, {10,6}, {5, 3}, {3,10}, {10,6}, {5, 3}, {3,10}, {10,6}, {5, 3}, {3,10}, {10,6}, {5, 3}, {3,10}, {10,6} });
 }
 
