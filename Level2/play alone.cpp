d53deedea3c3358d23114d79151a9cce63196d02
kkw2238@naver.com <kkw2238@naver.com>
2023-05-02 21:43:34 +0900

    2023.05.02 Lv.2 코드 풀이

---
+++ Level2/play alone.cpp
@@ -0,0 +1,40 @@
+/*
+    https://school.programmers.co.kr/learn/courses/30/lessons/131130
+*/
+
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int solution(vector<int> cards) {
+    int answer = 0, groupCount = 1;
+    vector<int> groupIndex(cards.size(), 0);
+
+    priority_queue<int> score;
+
+    for (int i = 0; i < cards.size(); ++i)
+    {
+        if (groupIndex[i] != 0)
+        {
+            continue;
+        }
+
+        int nextIndex = cards[i];
+        int counts = 1;
+        while (nextIndex != i)
+        {
+            groupIndex[nextIndex] = groupCount;
+            nextIndex = cards[nextIndex];
+        }
+
+        groupIndex[nextIndex] = groupCount;
+        score.push(counts);
+    }
+
+
+
+
+    return answer;
+}
