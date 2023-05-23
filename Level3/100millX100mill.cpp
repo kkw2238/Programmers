06a193e4e9ee165ea9ab5c65ff5520f2b1b46b02
kkw2238@naver.com <kkw2238@naver.com>
2023-05-23 21:32:33 +0900

    2023.05.23 Lv3 코드 풀이

--- Level3/100millX100mill.cpp
+++ Level3/100millX100mill.cpp
@@ -1,19 +1,57 @@
 /*
 	https://school.programmers.co.kr/learn/courses/30/lessons/138475?language=cpp
 */
+//
 
 #include <vector>
-#include <set>
-#include <cmath>
+#include <algorithm>
 
 using namespace std;
 
-vector<int> primeNumbers;
-vector<vector<byte>> biggestRanges;
+vector<int> solution(int e, vector<int> starts)
+{
+    vector<int> answer;
+    vector<int> divisorCounts(e + 2, 1);
+    vector<int> maximumDivisorIndex(e + 2, e);
 
-vector<set<int>> divisorCounts;
+    for (int i = 2; i <= e; ++i)
+    {
+        for (int j = 2; j <= e / i; ++j)
+        {
+            ++divisorCounts[i * j];
+        }
+    }
 
-void addDivisor(int n, int primeNumber)
+    for (int i = e; i >= 1; --i)
+    {
+        int index = maximumDivisorIndex[i + 1];
+
+        if (divisorCounts[index] <= divisorCounts[i])
+        {
+            index = i;
+        }
+        
+        maximumDivisorIndex[i] = index;
+    }
+
+    for (int index : starts)
+    {
+        answer.push_back(maximumDivisorIndex[index]);
+    }
+
+    return answer;
+}
+
+int main()
+{
+    solution(8, { 1,3,7 });
+}
+
+
+/*
+* vector<set<int>> divisorCounts;
+vector<int> divisorCounts2;
+    void addDivisor(int n, int primeNumber)
 {
     int childNum = n / primeNumber;
 
@@ -24,57 +62,7 @@ void addDivisor(int n, int primeNumber)
     }
 }
 
-void getDivisor(int n)
-{
-    for (int i : primeNumbers)
-    {
-        if (n % i == 0)
-        {
-            addDivisor(n, i);
-            return;
-        }
-    }
-
-    divisorCounts[n].insert(n);
-    primeNumbers.push_back(n);
-}
-
-vector<int> solution(int e, vector<int> starts) {
-    vector<int> answer;
-    divisorCounts = vector<set<int>>(e + 1, set<int>());
-
-    divisorCounts[1].insert(1);
-    for (int i = 2; i <= e; ++i)
-    {
-        divisorCounts[i].insert(1);
-        getDivisor(i);
-    }
-
-    for (int index : starts)
-    {
-        int maximum = 0;
-        int indexArray = 0;
-        for (int i = index; i <= e; ++i)
-        {
-            if (divisorCounts[i].size() > maximum)
-            {
-                maximum = divisorCounts[i].size();
-                indexArray = i;
-            }
-        }
-        answer.push_back(indexArray);
-    }
-
-    return answer;
-}
-
-
-int main()
-{
-    solution(8, { 1,3,7 });
-}
-
-
+*/
 
 //const bool isAblefactorization(int num, int primeNum)
 //{
@@ -125,6 +113,58 @@ int main()
 //            if (primeCounts[i] > maximum)
 //            {
 //                maximum = primeCounts[i];
+//                indexArray = i;
+//            }
+//        }
+//        answer.push_back(indexArray);
+//    }
+//
+//    return answer;
+//}
+
+
+//#include <vector>
+//#include <cmath>
+//
+//using namespace std;
+//
+//vector<int> divisorCounts;
+//
+//int getDivisor(int n)
+//{
+//    int result = 0;
+//    int sqrtNum = sqrt(n) + 1;
+//    for (int i = 1; i <= sqrtNum; ++i)
+//    {
+//        if (i * i == n)
+//        {
+//            result += 1;
+//        }
+//        else if(n % i == 0)
+//        {
+//            result += 2;
+//        }
+//    }
+//    return result;
+//}
+//
+//vector<int> solution(int e, vector<int> starts) {
+//    vector<int> answer;
+//    divisorCounts = vector<int>(e + 1, 1);
+//
+//    for (int index : starts)
+//    {
+//        int maximum = 0;
+//        int indexArray = 0;
+//        for (int i = index; i <= e; ++i)
+//        {
+//            if (divisorCounts[i] == 0)
+//            {
+//                divisorCounts[i] = getDivisor(i);
+//            }
+//            if (divisorCounts[i] > maximum)
+//            {
+//                maximum = divisorCounts[i];
 //                indexArray = i;
 //            }
 //        }
