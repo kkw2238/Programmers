740a4f574c85d3f4b452f38404e22ad8a222b8c1
kkw2238@naver.com <kkw2238@naver.com>
2023-07-05 20:24:56 +0900

    2023.07.05 Lv3 문제 풀이중

---
+++ Level3/avantgarde tiling.cpp
@@ -0,0 +1,14 @@
+/*
+    https://school.programmers.co.kr/learn/courses/30/lessons/181186
+*/
+
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 23;
+    return answer;
+}
