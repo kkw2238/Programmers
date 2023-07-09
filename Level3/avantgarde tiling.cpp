ba5242fb238f60e87e449a81e94f6bb96aaf8ef2
kkw2238@naver.com <kkw2238@naver.com>
2023-07-09 21:08:42 +0900

    2023.07.09 Lv3 문제 풀이

--- Level3/avantgarde tiling.cpp
+++ Level3/avantgarde tiling.cpp
@@ -3,12 +3,26 @@
 */
 
 
-#include <string>
 #include <vector>
 
 using namespace std;
 
 int solution(int n) {
-    int answer = 23;
-    return answer;
+    vector<int> dp{ 1, 1, 3, 10 };
+    vector<int> offset(3, 0);
+    int additional[3] = { 4, 2, 2 };
+
+    for (int i = 4; i <= n; ++i)
+    {
+        int next = (dp[i - 1] * 1 + dp[i - 2] * 2 + dp[i - 3] * 5) % 1000000007;
+        
+        for (int j = 0; j < 3; ++j)
+        {
+            offset[j] += dp[i - 4] * additional[j % 3];
+        }
+
+        dp.push_back((next + offset[i % 3]) % 1000000007);
+    }
+
+    return dp[n];
 }
