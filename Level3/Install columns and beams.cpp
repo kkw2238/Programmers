e9c62ba2423c1c59801d4b2e360e6d56fa2a1482
kkw2238@naver.com <kkw2238@naver.com>
2023-07-14 20:08:19 +0900

    2023.07.14 Lv3 문제 풀이

--- Level3/Install columns and beams.cpp
+++ Level3/Install columns and beams.cpp
@@ -4,13 +4,16 @@
 
 #include <string>
 #include <vector>
+#include <set>
 
 using namespace std;
 
 enum TYPE{ COLUMN = 1, BEAM = 2 };
 
 /*
+    기둥은 바닥 위에 있거나 보의 한쪽 끝 부분 위에 있거나, 또는 다른 기둥 위에 있어야 합니다.
     보는 한쪽 끝 부분이 기둥 위에 있거나, 또는 양쪽 끝 부분이 다른 보와 동시에 연결되어 있어야 합니다.
+
 */
 
 bool isAbleBuild(vector<vector<int>>& graph, int x, int y, int objectType)
@@ -18,32 +21,33 @@ bool isAbleBuild(vector<vector<int>>& graph, int x, int y, int objectType)
     switch (objectType)
     {
     case COLUMN:
-        return y == 0 || graph[y - 1][x] == COLUMN || (graph[y][x - 1] >= BEAM || graph[y][x] >= BEAM);
+        return y == 0 || graph[y - 1][x] % 2 == COLUMN || (graph[y][x - 1] >= BEAM || graph[y][x] >= BEAM);
     case BEAM:
-        return ((x > 0 && graph[y][x - 1] % 2 == BEAM) && graph[y][x + 1] % 2 == BEAM) || graph[y - 1][x] >= COLUMN || graph[y - 1][x + 1] >= COLUMN;
+        return ((x > 0 && graph[y][x - 1] >= BEAM) && graph[y][x + 1] >= BEAM) || graph[y - 1][x] % 2 == COLUMN || graph[y - 1][x + 1] % 2 == COLUMN;
     }
 }
 
-bool isAbleErase(vector<vector<int>>& graph, vector<vector<int>>& build_frame, int index, int objectType)
+bool isAbleErase(vector<vector<int>>& graph, set<pair<int, int>>& buildings, int index, int objectType)
 {
-    for (int i = 0; i < index; ++i)
+    for (const pair<int, int>& buildPos : buildings)
     {
-        if (build_frame[i][3] == 0)
+        if (graph[buildPos.second][buildPos.first] % 2 == COLUMN && !isAbleBuild(graph, buildPos.first, buildPos.second, COLUMN))
         {
-            continue;
+            return false;
         }
-        if (!isAbleBuild(graph, build_frame[i][0], build_frame[i][1], build_frame[i][2] + 1))
+        if (graph[buildPos.second][buildPos.first] >= BEAM && !isAbleBuild(graph, buildPos.first, buildPos.second, BEAM))
         {
             return false;
         }
     }
-
+    
     return true;
 }
 
 vector<vector<int>> solution(int n, vector<vector<int>> build_frame) {
     vector<vector<int>> graph(n + 2, vector<int>(n + 2, 0));
     vector<vector<int>> answer;
+    set<pair<int, int>> buildings;
 
     for (int i = 0; i < build_frame.size(); ++i)
     {
@@ -52,9 +56,13 @@ vector<vector<int>> solution(int n, vector<vector<int>> build_frame) {
         if (build_frame[i][3] == 0)
         {
             graph[build_frame[i][1]][build_frame[i][0]] -= objectType;
-            if (!isAbleErase(graph, build_frame, i, objectType))
+            if (!isAbleErase(graph, buildings, i, objectType))
             {
                 graph[build_frame[i][1]][build_frame[i][0]] += objectType;
+            }
+            else if (graph[build_frame[i][1]][build_frame[i][0]] == 0)
+            {
+                buildings.erase(pair(build_frame[i][0], build_frame[i][1]));
             }
         }
         else if (build_frame[i][3] == 1)
@@ -62,6 +70,7 @@ vector<vector<int>> solution(int n, vector<vector<int>> build_frame) {
             if (isAbleBuild(graph, build_frame[i][0], build_frame[i][1], objectType))
             {
                 graph[build_frame[i][1]][build_frame[i][0]] += objectType;
+                buildings.insert(pair(build_frame[i][0], build_frame[i][1]));
             }
         }
     }
