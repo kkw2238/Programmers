5a081cf34cc4b65c28645d6a380c068fb2275f5c
kkw2238@naver.com <kkw2238@naver.com>
2023-08-14 20:51:07 +0900

    2023.08.14 Lv3 문제 풀이

--- Level3/Ad insertion.cpp
+++ Level3/Ad insertion.cpp
@@ -24,7 +24,7 @@ string addZero(string str)
     return str.substr(str.length() - 2);
 }
 
-string reverseTime(int log)
+string reverseTime(long long log)
 {
     string hour = to_string(log / 3600);
     log = log % 3600;
@@ -50,88 +50,127 @@ vector<int> unpackLogs(string& log)
     
     return unpacked;
 }
-
 string solution(string play_time, string adv_time, vector<string> logs) {
     string answer = "";
-    vector<int> beginTime, endTime;
 
     int pt = convertTime(play_time);
     int at = convertTime(adv_time);
-    
+
+    vector<long long> timeline(pt + 1, 0);
+
     for (string& log : logs)
     {
         vector<int> unpacked = unpackLogs(log);
 
-        beginTime.push_back(unpacked[0]);
-        endTime.push_back(unpacked[1]);
+        ++timeline[unpacked[0]];
+        --timeline[unpacked[1]];
     }
 
-    sort(beginTime.begin(), beginTime.end());
-    sort(endTime.begin(), endTime.end());
+    int bti = 0;
+    long long memAnswer = 0;
+    long long sumTime = 0, nowWatching = 0, memTime = 0;
 
-    map<int, int> timeCount;
-    timeCount[0] = 0;
-    timeCount[pt - at] = 0;
-    int bti = 0, eti = 0;
-    int nowTime = beginTime[0], count = 0;
-    while (bti < logs.size())
+    for (int eti = 0; eti <= pt; ++eti)
     {
-        if (beginTime[bti] < endTime[eti])
+        nowWatching += timeline[eti];
+        sumTime += nowWatching;
+        timeline[eti] = nowWatching;
+        
+        if ((eti - bti) > at)
         {
-            timeCount[beginTime[bti++]] = ++count;
-        }
-        else if (beginTime[bti] > endTime[eti])
-        {
-            timeCount[endTime[eti++]] = --count;
-        }
-        else
-        {
-            ++eti;
-            ++bti;
-        }
-    }
-
-    for (int i = eti; i < logs.size(); ++i)
-    {
-        timeCount[endTime[i]] = --count;
-    }
-
-    beginTime.push_back(0);
-    beginTime.push_back(pt - at);
-
-    sort(beginTime.begin(), beginTime.end());
-
-    int memCount = 0, memAnswer = 0;
-    for (int i = 0; i < beginTime.size(); ++i)
-    {
-        int eTime = beginTime[i] + at;
-        int memTime = beginTime[i];
-        int count = 0;
-        auto iter = timeCount.find(beginTime[i]);
-        for (; iter != timeCount.end(); ++iter)
-        {
-            int elapsedTime = min(iter->first, eTime) - memTime;
-            count += timeCount[memTime] * elapsedTime;
-            memTime = iter->first;
-
-            if (iter->first > eTime)
+            if (sumTime > memTime)
             {
-                break;
+                memTime = sumTime;
+                memAnswer = bti;
             }
-        }
-
-        if (count > memCount)
-        {
-            memAnswer = beginTime[i];
-            memCount = count;
+            sumTime -= timeline[bti++];
         }
     }
-    answer = reverseTime(memAnswer);
 
     return reverseTime(memAnswer);
 }
 
+//string solution(string play_time, string adv_time, vector<string> logs) {
+//    string answer = "";
+//    vector<int> beginTime, endTime;
+//
+//    int pt = convertTime(play_time);
+//    int at = convertTime(adv_time);
+//
+//    for (string& log : logs)
+//    {
+//        vector<int> unpacked = unpackLogs(log);
+//
+//        beginTime.push_back(unpacked[0]);
+//        endTime.push_back(unpacked[1]);
+//    }
+//
+//    sort(beginTime.begin(), beginTime.end());
+//    sort(endTime.begin(), endTime.end());
+//
+//    map<int, int> timeCount;
+//    timeCount[0] = 0;
+//    timeCount[pt - at] = 0;
+//    int bti = 0, eti = 0;
+//    int count = 0;
+//    while (bti < logs.size())
+//    {
+//        if (beginTime[bti] <= endTime[eti])
+//        {
+//            timeCount[beginTime[bti++]] = ++count;
+//        }
+//        else if (beginTime[bti] > endTime[eti])
+//        {
+//            timeCount[endTime[eti++]] = --count;
+//        }
+//    }
+//
+//    for (int i = eti; i < logs.size(); ++i)
+//    {
+//        timeCount[endTime[i]] = --count;
+//    }
+//
+//    beginTime.push_back(0);
+//    beginTime.push_back(pt - at);
+//
+//    sort(beginTime.begin(), beginTime.end());
+//
+//    int memCount = 0, memAnswer = 0;
+//    for (int i = 0; i < beginTime.size(); ++i)
+//    {
+//        int eTime = beginTime[i] + at;
+//        int memTime = beginTime[i];
+//        int count = 0;
+//        auto iter = timeCount.find(beginTime[i]);
+//
+//        if (eTime > pt)
+//        {
+//            break;
+//        }
+//
+//        for (; iter != timeCount.end(); ++iter)
+//        {
+//            int elapsedTime = min(iter->first, eTime) - memTime;
+//            count += timeCount[memTime] * elapsedTime;
+//            memTime = iter->first;
+//
+//            if (iter->first > eTime)
+//            {
+//                break;
+//            }
+//        }
+//
+//        if (count > memCount)
+//        {
+//            memAnswer = beginTime[i];
+//            memCount = count;
+//        }
+//    }
+//
+//    return reverseTime(memAnswer);
+//}
+
 int main()
 {
-    solution("99:59:59", "25:00:00", { "69:59:59-89:59:59", "01:00:00-21:00:00", "79:59:59-99:59:59", "11:00:00-31:00:00" });
+    solution("02:03:55", "00:14:15", { "01:20:15-01:45:14", "00:40:31-01:00:00", "00:25:50-00:48:29", "01:30:59-01:53:29", "01:37:44-02:02:30" });
 }
