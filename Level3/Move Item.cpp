a2d0e48d4dd8e0389b8d05c737db80649c9b8a52
kkw2238@naver.com <kkw2238@naver.com>
2023-09-07 19:51:58 +0900

    2023.09.07 Lv3 문제 풀이

--- Level3/Move Item.cpp
+++ Level3/Move Item.cpp
@@ -87,145 +87,267 @@
 #include <iostream>
 
 
+//#include <queue>
+//#include <vector>
+//
+//using namespace std;
+//
+//int minimum = 100000;
+//int width = 0, height = 0;
+//int otherX[4]{ 1, 0, -1, 0 };
+//int otherY[4]{ 0, 1, 0, -1 };
+//
+//struct node {
+//    int x, y, type, count;
+//    bool isRot = false; 
+//};
+//
+//bool isAbleMove(const vector<vector<int>>& board, int x, int y, int type)
+//{
+//    if (x < 0 || x + otherX[type] < 0 || y < 0 || y + otherY[type] < 0)
+//    {
+//        return false;
+//    }
+//    if (x >= width || (x + otherX[type]) >= width || y >= height || (y + otherY[type]) >= height)
+//    {
+//        return false;
+//    }
+//
+//    if (board[y + otherY[type]][x + otherX[type]] == 1 || board[y][x] == 1)
+//    {
+//        return false;
+//    }
+//
+//    return true;
+//}
+//
+//bool isAbleRotate(const vector<vector<int>>& board, int x, int y, int beforeType, int type)
+//{
+//    switch (type)
+//    {
+//    case 0:
+//        if (beforeType == 1)
+//        {
+//            return isAbleMove(board, x + 1, y, 1);
+//        }
+//        else if (beforeType == 3)
+//        {
+//            return isAbleMove(board, x + 1, y, 3);
+//        }
+//    case 1:
+//        if (beforeType == 2)
+//        {
+//            return isAbleMove(board, x, y + 1, 2);
+//        }
+//        else if (beforeType == 0)
+//        {
+//            return isAbleMove(board, x, y + 1, 0);
+//        }
+//    case 2:
+//        if (beforeType == 1)
+//        {
+//            return isAbleMove(board, x - 1, y, 1);
+//        }
+//        else if (beforeType == 3)
+//        {
+//            return isAbleMove(board, x - 1, y, 3);
+//        }
+//    case 3:
+//        if (beforeType == 2)
+//        {
+//            return isAbleMove(board, x, y - 1, 2);
+//        }
+//        else if (beforeType == 0)
+//        {
+//            return isAbleMove(board, x, y - 1, 0);
+//        }
+//    }
+//}
+//
+//void BFS(const vector<vector<int>>& board, int x, int y, int type)
+//{
+//    bool footPrint[2][100][100] { false };
+//    queue<node> q;
+//    vector<vector<vector<int>>> tmp(2, vector<vector<int>>(board.size(), vector<int>(board[0].size(), 40)));
+//
+//    q.push({ x, y, type, 0, false });
+//
+//    while (!q.empty())
+//    {
+//        node n = q.front();
+//        q.pop();
+//
+//        node tmpN = n;
+//
+//        if (n.type < 2)
+//        {
+//            n.x = n.x + otherX[n.type];
+//            n.y = n.y + otherY[n.type];
+//            n.type = n.type + 2;
+//        }
+//        if (tmp[n.type % 2][n.y][n.x] < n.count  || footPrint[n.type % 2][n.y][n.x] && !n.isRot)
+//        {
+//            continue;
+//        }
+//        
+//        tmp[n.type % 2][n.y][n.x] = n.count;
+//
+//        footPrint[n.type % 2][n.y][n.x] = true;
+//
+//        if ((n.x == width - 1 && n.y == height - 1) || (n.x + otherX[n.type] == width - 1 && n.y + otherY[n.type] == height - 1))
+//        {
+//            minimum = min(minimum, n.count);
+//            continue;
+//        }
+//
+//        for (int i = 0; i < 4; ++i)
+//        {
+//            if (isAbleMove(board, n.x + otherX[i], n.y + otherY[i], n.type))
+//            {
+//                q.push({ n.x + otherX[i], n.y + otherY[i], n.type, n.count + 1, false });
+//            }
+//        }
+//
+//        if (!n.isRot)
+//        {
+//            if (isAbleRotate(board, n.x, n.y, n.type, (n.type + 5) % 4))
+//            {
+//                q.push({ n.x, n.y, (n.type + 5) % 4, n.count + 1, true });
+//            }
+//            if (isAbleRotate(board, n.x + otherX[n.type], n.y + otherY[n.type], (n.type + 2) % 4, (n.type + 3) % 4))
+//            {
+//                q.push({ n.x + otherX[n.type], n.y + otherY[n.type], (n.type + 3) % 4, n.count + 1, true });
+//            }
+//
+//            if (isAbleRotate(board, n.x, n.y, n.type, (n.type + 3) % 4))
+//            {
+//                q.push({ n.x, n.y, (n.type + 3) % 4, n.count + 1, true });
+//            }
+//            if (isAbleRotate(board, n.x + otherX[n.type], n.y + otherY[n.type], (n.type + 2) % 4, (n.type + 5) % 4))
+//            {
+//                q.push({ n.x + otherX[n.type], n.y + otherY[n.type], (n.type + 5) % 4, n.count + 1, true });
+//            }
+//        }
+//    }
+//}
+
 #include <queue>
 #include <vector>
 
 using namespace std;
 
-int minimum = 100000;
-int width = 0, height = 0;
-int otherX[4]{ 1, 0, -1, 0 };
-int otherY[4]{ 0, 1, 0, -1 };
-
 struct node {
     int x, y, type, count;
-    bool isRot = false; 
 };
 
-bool isAbleMove(const vector<vector<int>>& board, int x, int y, int type)
+int dirX[4]{ 1, -1, 0, 0 };
+int dirY[4]{ 0, 0, 1, -1 };
+
+bool isAbleMove(vector<vector<int>>& board, int x, int y, int type, int index)
 {
-    if (x < 0 || x + otherX[type] < 0 || y < 0 || y + otherY[type] < 0)
+    int otherX = type == 0 ? x - 1 : x;
+    int otherY = type == 1 ? y - 1 : y;
+
+    if (otherX < 0 || x >= board[0].size() || otherY < 0 || y >= board.size())
     {
         return false;
     }
-    if (x >= width || (x + otherX[type]) >= width || y >= height || (y + otherY[type]) >= height)
+    else if (otherX + dirX[index] < 0 || x + dirX[index] >= board[0].size() || otherY + dirY[index] < 0 || y + dirY[index] >= board.size())
     {
         return false;
     }
 
-    if (board[y + otherY[type]][x + otherX[type]] == 1 || board[y][x] == 1)
-    {
-        return false;
-    }
-
-    return true;
+    return board[y + dirY[index]][x + dirX[index]] == 0 && board[otherY + dirY[index]][otherX + dirX[index]] == 0;
 }
 
-bool isAbleRotate(const vector<vector<int>>& board, int x, int y, int beforeType, int type)
+int BFS(vector<vector<int>> board)
 {
-    switch (type)
+    bool footPrint[2][100][100]{ false };
+    int height = board.size(), width = board[0].size();
+    queue<node> qn;
+
+    qn.push({ 1, 0, 0, 0 });
+
+    while (!qn.empty())
     {
-    case 0:
-        return isAbleMove(board, x + 1, y, 3) || isAbleMove(board, x + 1, y, 1);
-    case 1:
-        return isAbleMove(board, x, y + 1, 2) || isAbleMove(board, x, y + 1, 0);
-    case 2:
-        return isAbleMove(board, x - 1, y, 3) || isAbleMove(board, x - 1, y, 1);
-    case 3:
-        return isAbleMove(board, x, y - 1, 2) || isAbleMove(board, x, y - 1, 0);
-    }
-}
-
-void BFS(const vector<vector<int>>& board, int x, int y, int type)
-{
-    bool footPrint[2][100][100] { false };
-    queue<node> q;
-    vector<vector<vector<int>>> tmp(2, vector<vector<int>>(board.size(), vector<int>(board[0].size(), 40)));
-
-    q.push({ x, y, type, 0, false });
-
-    while (!q.empty())
-    {
-        node n = q.front();
-        q.pop();
-
-        node tmpN = n;
-
-        if (n.type < 2)
-        {
-            n.x = n.x + otherX[n.type];
-            n.y = n.y + otherY[n.type];
-            n.type = n.type + 2;
-        }
-        if (tmp[n.type % 2][n.y][n.x] < n.count  || footPrint[n.type % 2][n.y][n.x] && !n.isRot)
+        node n = qn.front();
+        qn.pop();
+       
+        if (footPrint[n.type][n.y][n.x])
         {
             continue;
         }
-        
-        tmp[n.type % 2][n.y][n.x] = n.count;
 
-        footPrint[n.type % 2][n.y][n.x] = true;
-
-        if ((n.x == width - 1 && n.y == height - 1) || (n.x + otherX[n.type] == width - 1 && n.y + otherY[n.type] == height - 1))
+        if (n.y == board.size() - 1 && n.x == board[0].size() - 1)
         {
-            minimum = min(minimum, n.count);
-            continue;
+            return n.count;
+        }
+
+        footPrint[n.type][n.y][n.x] = true;
+
+        if (n.type == 0)
+        {
+            if (n.x > 0 && n.y > 0 && board[n.y - 1][n.x] == 0 && board[n.y - 1][n.x - 1] == 0)
+            {
+                qn.push({ n.x - 1, n.y, 1, n.count + 1 });
+                qn.push({ n.x, n.y, 1, n.count + 1 });
+            }
+            if (n.x < board[0].size() && n.y < board.size() - 1 && board[n.y + 1][n.x - 1] == 0 && board[n.y + 1][n.x] == 0)
+            {
+                qn.push({ n.x - 1, n.y + 1, 1, n.count + 1 });
+                qn.push({ n.x, n.y + 1, 1, n.count + 1 });
+            }
+        }
+        else if (n.type == 1)
+        {
+            if (n.x > 0 && n.y > 0 && board[n.y - 1][n.x - 1] == 0 && board[n.y][n.x - 1] == 0)
+            {
+                qn.push({ n.x, n.y, 0, n.count + 1 });
+                qn.push({ n.x, n.y - 1, 0, n.count + 1 });
+            }
+            if (n.x < board[0].size() - 1 && n.y < board[0].size() && board[n.y][n.x + 1] == 0 && board[n.y - 1][n.x + 1] == 0)
+            {
+                qn.push({ n.x + 1, n.y, 0, n.count + 1 });
+                qn.push({ n.x + 1, n.y - 1, 0, n.count + 1 });
+            }
         }
 
         for (int i = 0; i < 4; ++i)
         {
-            if (isAbleMove(board, n.x + otherX[i], n.y + otherY[i], n.type))
+            if (isAbleMove(board, n.x, n.y, n.type, i) && !footPrint[n.type][n.y + dirY[i]][n.x + dirX[i]])
             {
-                q.push({ n.x + otherX[i], n.y + otherY[i], n.type, n.count + 1, false });
+                qn.push({ n.x + dirX[i], n.y + dirY[i], n.type, n.count + 1 });
             }
-        }
-
-        if (!n.isRot && isAbleRotate(board, n.x, n.y, n.type, (n.type + 5) % 4))
-        {
-            q.push({ n.x, n.y, (n.type + 5) % 4, n.count + 1, true });
-        }
-
-        if (!n.isRot && isAbleRotate(board, n.x + otherX[n.type], n.y + otherY[n.type], (n.type + 2) % 4, (n.type + 3) % 4))
-        {
-            q.push({ n.x + otherX[n.type], n.y + otherY[n.type], (n.type + 3) % 4, n.count + 1, true });
-        }
-
-        if (!n.isRot && isAbleRotate(board, n.x, n.y, n.type, (n.type + 3) % 4))
-        {
-            q.push({ n.x, n.y, (n.type + 3) % 4, n.count + 1, true });
-        }
-        if (!n.isRot && isAbleRotate(board, n.x + otherX[n.type], n.y + otherY[n.type], (n.type + 2) % 4, (n.type + 5) % 4))
-        {
-            q.push({ n.x + otherX[n.type], n.y + otherY[n.type], (n.type + 5) % 4, n.count + 1, true });
         }
     }
 
-    for (int m = 0; m < 2; ++m)
-    {
-        for (int i = 0; i < height; ++i)
-        {
-            for (int j = 0; j < width; ++j)
-            {
-                cout << tmp[m][i][j] << ' ';
-            }
-            cout << '\n';
-        }
-        cout << '\n';
-    }
+    return 0;
 }
 
 int solution(vector<vector<int>> board) {
-    int answer = 0;
-    height = board.size();
-    width = board[0].size();
-
-    BFS(board, 1, 0, 2);
-
-    return minimum;
+    return BFS(board);
 }
+
 
 int main()
 {
+    //solution("handshake", "shake hands");
+    
+
+    solution(
+        [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 1, 1, 1, 1, 1, 0, 0],
+        [1, 1, 1, 1, 1, 1, 1, 1, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 1, 1, 1, 1, 0, 0],
+        [0, 1, 1, 1, 1, 1, 1, 1, 1],
+        [0, 0, 1, 1, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 1, 1, 1, 1, 1, 1, 0]]
+
+    );
+
     solution({
         {0, 0, 0, 0, 0, 0, 1},
         {1, 1, 1, 1, 0, 0, 1},
---
+++ Level3/Reverse Coin.cpp
@@ -0,0 +1,127 @@
+/*
+	https://school.programmers.co.kr/learn/courses/30/lessons/131703
+*/
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int minimum = 100000;
+int width, height;
+
+void reverseColumn(vector<vector<int>>& board, int index)
+{
+    for (int i = 0; i < height; ++i)
+    {
+        board[i][index] = board[i][index] == 0 ? 1 : 0;
+    }
+}
+
+void reverseRow(vector<vector<int>>& board, int index)
+{
+    int y = index - width;
+    for (int i = 0; i < width; ++i)
+    {
+        board[y][i] = board[y][i] == 0 ? 1 : 0;
+    }
+}
+
+bool isCorrect(vector<vector<int>>& board, const vector<vector<int>>& target, int index)
+{
+    if (index <= width)
+    {
+        return true;
+    }
+
+    for (int y = 0; y < index - width; ++y)
+    {
+        for (int x = 0; x < width; ++x)
+        {
+            if (board[y][x] != target[y][x])
+            {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+
+void DFS(vector<vector<int>>& board, const vector<vector<int>>& target, int index, int count)
+{
+    if (index >= width)
+    {
+        bool correct = isCorrect(board, target, index);
+        
+        if (correct && (index == width + height))
+        {
+            minimum = min(minimum, count);
+
+            return;
+        }
+        else if (!correct)
+        {
+            return;
+        }
+    }
+
+    if (index >= width)
+    {
+        reverseRow(board, index);
+        DFS(board, target, index + 1, count + 1);
+        reverseRow(board, index);
+    }
+    else
+    {
+        reverseColumn(board, index);
+        DFS(board, target, index + 1, count + 1);
+        reverseColumn(board, index);
+    }
+
+    DFS(board, target, index + 1, count);
+}
+
+int solution(vector<vector<int>> beginning, vector<vector<int>> target) {
+    width = target[0].size();
+    height = target.size();
+
+    DFS(beginning, target, 0, 0);
+
+    if (minimum == 100000)
+    {
+        return -1;
+    }
+
+    return minimum;
+}
+
+int main()
+{
+    solution({
+        {0, 0, 1, 0, 0, 1, 0},
+        {1, 0, 0, 0, 0, 1, 0},
+        {0, 0, 0, 0, 0, 1, 0},
+        {0, 0, 0, 0, 0, 1, 0},
+        {0, 0, 0, 0, 0, 1, 0},
+        {0, 0, 1, 0, 0, 1, 0},
+        {1, 0, 0, 0, 0, 1, 0},
+        {0, 0, 0, 0, 0, 1, 0},
+        {0, 0, 0, 0, 0, 1, 0},
+        {0, 0, 0, 0, 0, 1, 0}
+        },
+
+    {
+        {0, 0, 1, 0, 0, 1, 0},
+        {1, 0, 0, 0, 0, 1, 0},
+        {0, 0, 0, 0, 0, 1, 0},
+        {0, 0, 0, 0, 0, 1, 0},
+        {0, 0, 0, 0, 0, 1, 0},
+        {0, 0, 1, 0, 0, 1, 0},
+        {1, 0, 0, 0, 0, 1, 0},
+        {0, 0, 0, 0, 0, 1, 0},
+        {0, 0, 0, 0, 0, 1, 0},
+        {0, 0, 0, 0, 0, 1, 0}
+    });
+}
