f0e455a4ba92980ade36408fedde6fb9ff80a7a2
kkw2238@naver.com <kkw2238@naver.com>
2024-01-16 21:37:17 +0900

    2024.01.16 Lv3 문제 풀이

--- Kakao/2024Kakao Intern/2024KakaoIntern-03.cpp
+++ Kakao/2024Kakao Intern/2024KakaoIntern-03.cpp
@@ -2,7 +2,6 @@
 	https://school.programmers.co.kr/learn/courses/30/lessons/258709?language=cpp
 */
 
-#include <string>
 #include <vector>
 #include <bitset>
 #include <set>
@@ -19,10 +18,9 @@ void combination(vector<vector<int>>& dice, vector<int>& pos, int index, set<int
         return;
     }
 
-    int diceIndex = pos[index];
     for (int i = 0; i < 6; ++i)
     {
-        combination(dice, pos, index + 1, s, c, nowSum + dice[index][i]);
+        combination(dice, pos, index + 1, s, c, nowSum + dice[pos[index]][i]);
     }
 }
 
@@ -56,12 +54,17 @@ pair<int, int> getWinCount(set<int>& sumL, map<int, int>& lCount, set<int>& sumR
 int search(vector<vector<int>>& dice, vector<int>& lists, vector<vector<int>>& pos)
 {
     int maximumIndex = pow(2, dice.size()) - 1;
-    int maximum = 0;
-    int index = 0;
-    vector<int> winCount(maximumIndex + 1);
+    int maximum = 0, index = 0;
+
+    vector<int> winCount(maximumIndex + 1 , -1);
 
     for (int i = 0; i < lists.size(); ++i)
     {
+        if (winCount[lists[i]] >= 0)
+        {
+            continue;
+        }
+
         set<int> sumL, sumR;
         map<int, int> sumCountL, sumCountR;
         int otherSide = maximumIndex - lists[i];
@@ -90,12 +93,11 @@ int search(vector<vector<int>>& dice, vector<int>& lists, vector<vector<int>>& p
 }
 
 vector<int> solution(vector<vector<int>> dice) {
-    vector<int> answer;
-
     int maximumIndex = pow(2, dice.size());
     int minimumIndex = pow(2, dice.size() / 2) - 1;
 
     vector<vector<int>> position(maximumIndex);
+    vector<vector<int>> answer(maximumIndex);
     vector<int> lists;
 
     for (int i = minimumIndex; i < maximumIndex; ++i)
@@ -109,6 +111,7 @@ vector<int> solution(vector<vector<int>> dice) {
                 if (bs[j] == 1)
                 {
                     position[i].push_back(j);
+                    answer[i].push_back(j + 1);
                 }
             }
 
@@ -116,11 +119,6 @@ vector<int> solution(vector<vector<int>> dice) {
         }
     }
 
-    search(dice, lists, position);
-    return answer;
-}
-
-int main()
-{
-    solution({{1, 2, 3, 4, 5, 6}, {3, 3, 3, 3, 4, 4}, {1, 3, 3, 4, 4, 4}, {1, 1, 4, 4, 5, 5}} );
+    int index = search(dice, lists, position);
+    return position[index];
 }
