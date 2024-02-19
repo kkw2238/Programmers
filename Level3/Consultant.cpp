8cdc9b2e86ffea2ac442e7d99f6d5b1b80d94a7c
kkw2238@naver.com <kkw2238@naver.com>
2024-02-19 17:12:33 +0900

    2024.02.19 Lv3 문제 풀이

--- Level3/Consultant.cpp
+++ Level3/Consultant.cpp
@@ -2,57 +2,135 @@
 	https://school.programmers.co.kr/learn/courses/30/lessons/214288
 */
 
+//#include <vector>
+//#include <queue>
+//
+//using namespace std;
+//
+//struct consultant {
+//    consultant() = default;
+//    consultant(int a, int b)
+//    {
+//        begTime = a;
+//        time = b;
+//        endTime = begTime + time;
+//    }
+//    int begTime;
+//    int endTime;
+//    int time;
+//
+//    const bool operator<(const consultant& other) const
+//    {
+//        return begTime > other.begTime;
+//    }
+//};
+//
+//int getWaitTime(int consultantCount, vector<consultant>& student)
+//{
+//    int waitTime = 0;
+//    priority_queue<consultant> consultants;
+//
+//    for (int i = 0; i < consultantCount; ++i)
+//    {
+//        if (i < student.size()) 
+//        {
+//            consultants.push(consultant(student[i].begTime, student[i].time));
+//        }
+//        else
+//        {
+//            consultants.push(consultant());
+//        }
+//    }
+//
+//    for (int i = consultantCount; i < student.size(); ++i)
+//    {
+//        consultant c = consultants.top();
+//        consultants.pop();
+//
+//        if (c.endTime > student[i].begTime)
+//        {
+//            waitTime += c.endTime - student[i].begTime;
+//        }
+//
+//        consultants.push(consultant(c.endTime, student[i].time));
+//    }
+//
+//    return waitTime;
+//}
+//
+//int solution(int k, int n, vector<vector<int>> reqs) {
+//    int answer = 0;
+//    vector<vector<consultant>> students(k + 1);
+//    vector<int> consultantCount(k + 1, 1);
+//    vector<vector<int>> durationTime(k + 2, vector<int>(n + 1, -1));
+//
+//    n -= k;
+//
+//    for (vector<int>& req : reqs)
+//    {
+//        students[req[2]].emplace_back(req[0], req[1]);
+//    }
+//
+//    for (int i = 1; i <= k; ++i)
+//    {
+//        int result = getWaitTime(consultantCount[i], students[i]);
+//        durationTime[i][1] = result;
+//    }
+//
+//    while (--n >= 0)
+//    {
+//        int memWaitTime = 0;
+//        int requireType = 0;
+//
+//        for (int i = 1; i <= k; ++i)
+//        {
+//            if (durationTime[i][consultantCount[i] + 1] == -1)
+//            {
+//                durationTime[i][consultantCount[i] + 1] = getWaitTime(consultantCount[i] + 1, students[i]);
+//            }
+//
+//            int reduceTime = durationTime[i][consultantCount[i]] - durationTime[i][consultantCount[i] + 1];
+//            if (memWaitTime < reduceTime)
+//            {
+//                memWaitTime = reduceTime;
+//                requireType = i;
+//            }
+//        }
+//
+//        ++consultantCount[requireType];
+//    }
+//
+//    for (int i = 1; i <= k; ++i)
+//    {
+//        answer += durationTime[i][consultantCount[i]];
+//    }
+//
+//    return answer;
+//}
+
+#include <string>
 #include <vector>
 #include <queue>
 
 using namespace std;
 
-struct consultant {
-    consultant() = default;
-    consultant(int a, int b)
-    {
-        begTime = a;
-        time = b;
-        endTime = begTime + time;
-    }
-    int begTime;
-    int endTime;
-    int time;
-
-    const bool operator<(const consultant& other) const
-    {
-        return endTime > other.endTime;
-    }
-};
-
-int getWaitTime(int consultantCount, vector<consultant>& student)
+int waitTime(int consultantCount, vector<vector<int>>& students)
 {
     int waitTime = 0;
-    priority_queue<consultant> consultants;
+    priority_queue<int, vector<int>, greater<int>> consultants;
 
     for (int i = 0; i < consultantCount; ++i)
     {
-        if (i < student.size()) 
-        {
-            consultants.push(consultant(student[i].begTime, student[i].time));
-        }
-        else
-        {
-            consultants.push(consultant());
-        }
+        consultants.push(0);
     }
 
-    for (int i = consultantCount; i < student.size(); ++i)
+    for (vector<int>& student : students)
     {
-        consultant c = consultants.top();
+        int nowTime = consultants.top();
         consultants.pop();
 
-        if (c.endTime > student[i].begTime)
-        {
-            waitTime += c.endTime - student[i].begTime;
-        }
-
-        consultants.push(consultant(c.endTime, student[i].time));
+        waitTime += max(0, nowTime - student[0]);
+        consultants.push(max(nowTime, student[0]) + student[1]);
     }
 
     return waitTime;
@@ -60,55 +138,53 @@ int getWaitTime(int consultantCount, vector<consultant>& student)
 
 int solution(int k, int n, vector<vector<int>> reqs) {
     int answer = 0;
-    vector<vector<consultant>> students(k + 1);
+
     vector<int> consultantCount(k + 1, 1);
-    vector<vector<int>> durationTime(k + 2, vector<int>(n + 1, -1));
+    vector<vector<vector<int>>> students(k + 1, vector<vector<int>>());
+    vector<vector<int>> memConsultantTime(k + 1, vector<int>(n + 2, -1));
 
     n -= k;
 
     for (vector<int>& req : reqs)
     {
-        students[req[2]].emplace_back(req[0], req[1]);
+        students[req[2]].push_back({ req[0], req[1] });
     }
 
     for (int i = 1; i <= k; ++i)
     {
-        int result = getWaitTime(consultantCount[i], students[i]);
-        durationTime[i][1] = result;
+        memConsultantTime[i][1] = waitTime(1, students[i]);
     }
 
-    while (--n >= 0)
+    while (n-- > 0)
     {
-        int memWaitTime = 0;
-        int requireType = 0;
+        int memDiffTime = 0;
+        int memIndex = 0;
 
         for (int i = 1; i <= k; ++i)
         {
-            if (durationTime[i][consultantCount[i] + 1] == -1)
+            int conCount = consultantCount[i];
+            
+            if (memConsultantTime[i][conCount + 1] == -1)
             {
-                durationTime[i][consultantCount[i] + 1] = getWaitTime(consultantCount[i] + 1, students[i]);
+                memConsultantTime[i][conCount + 1] = waitTime(conCount + 1, students[i]);
             }
 
-            int reduceTime = durationTime[i][consultantCount[i]] - durationTime[i][consultantCount[i] + 1];
-            if (memWaitTime < reduceTime)
+            int diff = memConsultantTime[i][conCount] - memConsultantTime[i][conCount + 1];
+            
+            if (diff > memDiffTime)
             {
-                memWaitTime = reduceTime;
-                requireType = i;
+                memDiffTime = diff;
+                memIndex = i;
             }
         }
 
-        ++consultantCount[requireType];
+        ++consultantCount[memIndex];
     }
 
     for (int i = 1; i <= k; ++i)
     {
-        answer += durationTime[i][consultantCount[i]];
+        answer += memConsultantTime[i][consultantCount[i]];
     }
 
     return answer;
-}
-
-int main()
-{
-    solution(5, 5, { { 10, 60, 1},{15, 100, 3},{20, 30, 1},{30, 50, 3},{50, 40, 1},{60, 30, 2},{65, 30, 1},{70, 100, 2} });
 }
--- Level3/vanishing platform.cpp
+++ Level3/vanishing platform.cpp
@@ -2,70 +2,107 @@
 	https://school.programmers.co.kr/learn/courses/30/lessons/92345
 */
 
+//#include <vector>
+//
+//using namespace std;
+//
+//int dirX[4]{ -1, 1, 0, 0 };
+//int dirY[4]{ 0, 0, -1, 1 };
+//
+//int width = 0;
+//int height = 0;
+//int maximum = 0;
+//
+//bool isAbleMove(int x, int y)
+//{
+//    return x >= 0 && x < width && y >= 0 && y < height;
+//}
+//
+//void DFS(vector<vector<int>>& board, int ax, int ay, int bx, int by, int mCount)
+//{
+//    if (board[ay][ax] == 0 || board[by][bx] == 0)
+//    {
+//        return;
+//    }
+//    
+//    
+//    for (int i = 0; i < 4; ++i)
+//    {
+//        int nextAX = ax + dirX[i];
+//        int nextAY = ay + dirY[i];
+//
+//        board[ay][ax] = 0;
+//        if (!isAbleMove(nextAX, nextAY) || board[nextAY][nextAX] == 0)
+//        {
+//            continue;
+//        }
+//    
+//        for (int j = 0; j < 4; ++j)
+//        {
+//            int nextBX = bx + dirX[j];
+//            int nextBY = by + dirY[j];
+//
+//            if (!isAbleMove(nextBX, nextBY) || board[nextBY][nextBX] == 0)
+//            {
+//                continue;
+//            }
+//            else if (board[by][bx] == 0)
+//            {
+//                break;
+//            }
+//
+//            board[by][bx] = 0;
+//            DFS(board, nextAX, nextAY, nextBX, nextBY, mCount + 1);
+//        }
+//    }
+//    board[ay][ax] = 1;
+//    board[by][bx] = 1;
+//
+//    maximum = max(maximum, mCount);
+//}
+//
+//int solution(vector<vector<int>> board, vector<int> aloc, vector<int> bloc) {
+//    width = board[0].size();
+//    height = board.size();
+//
+//    DFS(board, aloc[1], aloc[0], bloc[1], bloc[0], 0);
+//
+//    return maximum;
+//}
+
+
 #include <vector>
 
 using namespace std;
 
+int width = 0, height = 0;
+int aWinCount = 0, bWinCount = 0;
+int aWinMoveCount = 0, bWinMoveCount = 0;
+
 int dirX[4]{ -1, 1, 0, 0 };
 int dirY[4]{ 0, 0, -1, 1 };
 
-int width = 0;
-int height = 0;
-int maximum = 0;
+vector<vector<int>> footPrint;
 
-bool isAbleMove(int x, int y)
+void DFS(vector<vector<int>>& board, int ax, int ay, int aMoveCount, int bx, int by, int bMoveCount)
 {
-    return x >= 0 && x < width && y >= 0 && y < height;
-}
-
-void DFS(vector<vector<int>>& board, int ax, int ay, int bx, int by, int mCount)
-{
-    if (board[ay][ax] == 0 || board[by][bx] == 0)
-    {
-        return;
-    }
-    
-    
     for (int i = 0; i < 4; ++i)
     {
-        int nextAX = ax + dirX[i];
-        int nextAY = ay + dirY[i];
-
-        board[ay][ax] = 0;
-        if (!isAbleMove(nextAX, nextAY) || board[nextAY][nextAX] == 0)
-        {
-            continue;
-        }
-    
         for (int j = 0; j < 4; ++j)
         {
-            int nextBX = bx + dirX[j];
-            int nextBY = by + dirY[j];
 
-            if (!isAbleMove(nextBX, nextBY) || board[nextBY][nextBX] == 0)
-            {
-                continue;
-            }
-            else if (board[by][bx] == 0)
-            {
-                break;
-            }
-
-            board[by][bx] = 0;
-            DFS(board, nextAX, nextAY, nextBX, nextBY, mCount + 1);
         }
     }
-    board[ay][ax] = 1;
-    board[by][bx] = 1;
 
-    maximum = max(maximum, mCount);
 }
 
 int solution(vector<vector<int>> board, vector<int> aloc, vector<int> bloc) {
     width = board[0].size();
     height = board.size();
 
-    DFS(board, aloc[1], aloc[0], bloc[1], bloc[0], 0);
+    footPrint = vector<vector<int>>(height, vector<int>(width, 0));
+
+    DFS(board, aloc[0], aloc[1], 0, bloc[0], bloc[1], 0);
 
     return maximum;
 }
