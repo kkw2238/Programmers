3bec5bcdd1c6f8be2ceede5513fbb8876010093a
kkw2238@naver.com <kkw2238@naver.com>
2024-03-04 21:13:32 +0900

    2024.03.04 Lv3 문제 풀이

--- Level3/vanishing platform.cpp
+++ Level3/vanishing platform.cpp
@@ -70,44 +70,145 @@
 //    return maximum;
 //}
 
+/*
+    [
+    [1, 1, 1, 0], 
+    [1, 1, 0, 1], 
+    [1, 0, 1, 1], 
+    [0, 1, 1, 1]]
+*/
 
+#include <iostream>
 #include <vector>
 
 using namespace std;
 
 int width = 0, height = 0;
-int aWinCount = 0, bWinCount = 0;
-int aWinMoveCount = 0, bWinMoveCount = 0;
+int aWinCount = 0, maximumTurn = 0;
+bool isLink = false;
 
 int dirX[4]{ -1, 1, 0, 0 };
 int dirY[4]{ 0, 0, -1, 1 };
 
 vector<vector<int>> footPrint;
 
-void DFS(vector<vector<int>>& board, int ax, int ay, int aMoveCount, int bx, int by, int bMoveCount)
+bool isInside(int x, int y)
 {
+    return x >= 0 && x < width && y >= 0 && y < height;
+}
+
+vector<int> getAbleMoveIndex(vector<vector<int>>& board, int x, int y)
+{
+    vector<int> result;
+
     for (int i = 0; i < 4; ++i)
     {
-        for (int j = 0; j < 4; ++j)
-        {
+        int nax = x + dirX[i];
+        int nay = y + dirY[i];
 
+        if (isInside(nax, nay) && footPrint[nay][nax] == 1 && board[nay][nax] == 1)
+        {
+            result.push_back(i);
         }
     }
 
+    return result;
+}
+
+int Play(vector<vector<int>>& board, int ax, int ay, int bx, int by, int moveCount, bool aTurn)
+{
+    vector<int> ableIndex = getAbleMoveIndex(board, ax, ay);
+
+    if (ableIndex.empty())
+    {
+        return moveCount;
+    }
+    else if (ax == bx && ay == by)
+    {
+        return moveCount + 1;
+    }
+
+    footPrint[ay][ax] = 0;
+    
+    int result = moveCount;
+    bool isWin = false;
+
+    for (int i : ableIndex)
+    {
+        int nx = ax + dirX[i];
+        int ny = ay + dirY[i];
+
+        int dfsResult = Play(board, bx, by, nx, ny, moveCount + 1, !aTurn);
+        
+        if (dfsResult % 2 == moveCount % 2)
+        {
+            if (result % 2 == dfsResult % 2)
+            {
+                result = min(result, dfsResult);
+            }
+            else
+            {
+                result = dfsResult;
+            }
+        }
+        else
+        {
+            result = max(result, dfsResult);
+        }
+        
+    }
+    footPrint[ay][ax] = 1;
+    
+    return result + 1;
 }
 
 int solution(vector<vector<int>> board, vector<int> aloc, vector<int> bloc) {
     width = board[0].size();
     height = board.size();
 
-    footPrint = vector<vector<int>>(height, vector<int>(width, 0));
+    footPrint = vector<vector<int>>(height, vector<int>(width, 1));
 
-    DFS(board, aloc[0], aloc[1], 0, bloc[0], bloc[1], 0);
-
-    return maximum;
+    return Play(board, aloc[1], aloc[0], bloc[1], bloc[0], 0, true);
 }
 
 int main()
 {
-    solution({ {1, 1, 1},{1, 1, 1 }, {1,1,1} } , { 1, 0 }, { 1, 2 });
+    cout << solution({ {1, 1, 1, 0 },{1, 1, 0, 1 }, {1, 0, 1,1}, {0,1,1,1} }, { 0, 0 }, { 3, 3 }) << '\n';
 }
+
+/*
+    
+void DFS(vector<vector<int>>& board, int ax, int ay, int bx, int by, int turnCount, bool aTurn)
+{
+    vector<int> ableAIndex = getAbleMoveIndex(board, ax, ay);
+    
+    if (ax == bx && ay == by)
+    {
+        if (aTurn && !ableAIndex.empty())
+        {
+            ++aWinCount;
+        }
+        isLink = true;
+        return;
+    }
+    else if (ableAIndex.empty())
+    {
+        if (!aTurn)
+        {
+            ++aWinCount;
+        }
+
+        maximumTurn = max(maximumTurn, turnCount);
+        return;
+    }
+    
+    footPrint[ay][ax] = 0;
+
+    for (int i : ableAIndex)
+    {
+        DFS(board, bx, by, ax + dirX[i], ay + dirY[i], turnCount + 1, !aTurn);
+    }
+
+    footPrint[ay][ax] = 1;
+}
+*/
