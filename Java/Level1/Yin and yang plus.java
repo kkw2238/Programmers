c3e5ae77ec2f26882aeb875c436d288ab9e862c4
kkw2238 <kkw2238@naver.com>
2024-06-21 23:02:08 +0900

    2024.06.21 Java SQL 문제 풀이

---
+++ Java/Level1/Yin and yang plus.java
@@ -0,0 +1,16 @@
+/*
+	https://school.programmers.co.kr/learn/courses/30/lessons/76501?language=java
+*/
+
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        
+        for(int i = 0; i < absolutes.length; ++i)
+        {
+            answer += signs[i] ? absolutes[i] : -absolutes[i];
+        }
+        
+        return answer;
+    }
+}
