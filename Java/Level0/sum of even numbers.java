1fcc9a8c9674111430743ff78fa4b641152897ba
kkw2238 <kkw2238@naver.com>
2024-07-18 17:17:22 +0900

    2024.07.18 Java 코드 수정2

--- Java/Level0/sum of even numbers.java
+++ Java/Level0/sum of even numbers.java
@@ -2,6 +2,8 @@
 	https://school.programmers.co.kr/learn/courses/30/lessons/120831
 */
 
-int solution(int n) {
-    return (n / 2) * (2 + ((n / 2) * 2)) / 2;
+class Solution {
+    public int solution(int n) {
+        return (n / 2) * (2 + ((n / 2) * 2)) / 2;
+    }
 }
